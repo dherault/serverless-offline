bb5ee5cf649070f41071376612fee1c34e7eb9dc
dherault <david.herault@edge-factory.com>
2016-05-23 13:52:55 +0200

    lint

--- src/createAuthScheme.js
+++ src/createAuthScheme.js
@@ -22,79 +22,76 @@ module.exports = function createAuthScheme(authFun, funName, endpointPath, optio
   }
 
   const authorizerOptions = populatedAuthFun.authorizer;
-  if(authorizerOptions.type !== 'TOKEN') {
+  if (authorizerOptions.type !== 'TOKEN') {
     throw new Error(`Authorizer Type must be TOKEN (λ: ${authFunName})`);
   }
 
-  if(authorizerOptions.identitySource !== 'method.request.header.Authorization') {
+  if (authorizerOptions.identitySource !== 'method.request.header.Authorization') {
     throw new Error(`Serverless Offline only supports retrieving tokens from the Authorization header (λ: ${authFunName})`);
   }
 
   const funOptions = functionHelper.getFunctionOptions(authFun, populatedAuthFun);
 
   // Create Auth Scheme
-  const scheme = function (server, schemeOptions) {
-      return {
-          authenticate: function (request, reply) {
-            console.log(''); // Just to make things a little pretty
-            serverlessLog(`Running Authorization function for ${request.method} ${request.path} (λ: ${authFunName})`);
+  return () => ({
 
-            // Get Authorization header
-            const req = request.raw.req;
-            const authorization = req.headers.authorization;
+    authenticate(request, reply) {
+      console.log(''); // Just to make things a little pretty
+      serverlessLog(`Running Authorization function for ${request.method} ${request.path} (λ: ${authFunName})`);
 
-            debugLog(`Recieved auth header: `, authorization);
-            let authToken = null;
+      // Get Authorization header
+      const req = request.raw.req;
+      const authorization = req.headers.authorization;
 
-            // Split auth header i.e 'Bearer xyzAbc...'
-            if (authorization) {
-              const authParts = authorization.split(' ');
-              authToken = authParts[1]; // take the token part of the header
-            }
+      debugLog(`Recieved auth header: ${authorization}`);
+      let authToken = null;
 
-            // Create event Object for authFunction
-            //   methodArn is the ARN of the function we are running we are authorizing access to (or not)
-            //   Account ID and API ID are not simulated
-            const event = {
-              "type": "TOKEN",
-              "authorizationToken": authToken,
-              "methodArn": `arn:aws:execute-api:${options.region}:<Account id>:<API id>/${options.stage}/${funName}/${endpointPath}`
-            };
+      // Split auth header i.e 'Bearer xyzAbc...'
+      if (authorization) {
+        const authParts = authorization.split(' ');
+        authToken = authParts[1]; // take the token part of the header
+      }
 
-            // Create the Authorization function handler
-            let handler;
-
-            try {
-              handler = functionHelper.createHandler(funOptions, options);
-            } catch (err) {
-              return reply(Boom.badImplementation(null, `Error while loading ${authFunName}`));
-            }
-
-            // Creat the Lambda Context for the Auth function
-            const lambdaContext = createLambdaContext(authFun, (err, policy) => {
-              // Return an unauthorized response
-              if(err) {
-                serverlessLog(`Authorization function returned an error response: (λ: ${authFunName})`, err);
-                return reply(Boom.forbidden('Forbidden'));
-              }
-
-              // Validate that the policy document has the principalId set
-              if(!policy.principalId) {
-                serverlessLog(`Authorization response did not include a principalId: (λ: ${authFunName})`, err);
-                return reply(Boom.forbidden('No principalId set on the Response'));
-              }
-
-              serverlessLog(`Authorization function returned a successful response: (λ: ${authFunName})`, policy);
-
-              // Set the credentials for the rest of the pipeline
-              return reply.continue({ credentials: { user: policy.principalId } });
-            });
-
-            // Execute the Authorization Function
-            handler(event, lambdaContext, lambdaContext.done);
-          }
+      // Create event Object for authFunction
+      //   methodArn is the ARN of the function we are running we are authorizing access to (or not)
+      //   Account ID and API ID are not simulated
+      const event = {
+        type: 'TOKEN',
+        authorizationToken: authToken,
+        methodArn: `arn:aws:execute-api:${options.region}:<Account id>:<API id>/${options.stage}/${funName}/${endpointPath}`,
       };
-  };
 
-  return scheme;
-}
+      // Create the Authorization function handler
+      let handler;
+
+      try {
+        handler = functionHelper.createHandler(funOptions, options);
+      } catch (err) {
+        return reply(Boom.badImplementation(null, `Error while loading ${authFunName}`));
+      }
+
+      // Creat the Lambda Context for the Auth function
+      const lambdaContext = createLambdaContext(authFun, (err, policy) => {
+        // Return an unauthorized response
+        if (err) {
+          serverlessLog(`Authorization function returned an error response: (λ: ${authFunName})`, err);
+          return reply(Boom.forbidden('Forbidden'));
+        }
+
+        // Validate that the policy document has the principalId set
+        if (!policy.principalId) {
+          serverlessLog(`Authorization response did not include a principalId: (λ: ${authFunName})`, err);
+          return reply(Boom.forbidden('No principalId set on the Response'));
+        }
+
+        serverlessLog(`Authorization function returned a successful response: (λ: ${authFunName})`, policy);
+
+        // Set the credentials for the rest of the pipeline
+        return reply.continue({ credentials: { user: policy.principalId } });
+      });
+
+      // Execute the Authorization Function
+      handler(event, lambdaContext, lambdaContext.done);
+    },
+  });
+};
