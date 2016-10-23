eb19a1773f9206e8c211f2772317cb8db7187397
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2016-10-23 13:45:02 -0300

    Add example for lambda integration type in manual test

--- manual_test/handler.js
+++ manual_test/handler.js
@@ -8,4 +8,8 @@ module.exports.hello = (event, context, cb) => {
   cb(null, response);
 };
 
+module.exports.helloLambdaIntegration = (event, context, cb) => {
+  cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
+
 // You can add more handlers here, and reference them in serverless.yml
