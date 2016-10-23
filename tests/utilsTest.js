68fd044f939b0a0b493684b4151216482abda7c4
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2016-10-23 19:29:49 -0300

    Add test for utils.toPlainOrEmptyObject

--- tests/utilsTest.js
+++ tests/utilsTest.js
@@ -7,8 +7,23 @@ const utils = require('../src/utils');
 const expect = chai.expect;
 chai.use(dirtyChai);
 
-
 describe('utils', () => {
+  describe('#toPlainOrEmptyObject', () => {
+    describe('with a plain object', () => {
+      it('should return the plain object', () => {
+        const plainObject = { name: 'Leonardo' };
+        expect(utils.toPlainOrEmptyObject(plainObject)).to.eq(plainObject);
+      });
+    });
+
+    describe('with a non plain object', () => {
+      it('should return an empty object', () => {
+        const nonPlainObject = [];
+        expect(utils.toPlainOrEmptyObject(nonPlainObject)).to.eql({});
+      });
+    });
+  });
+
   describe('#nullIfEmpty', () => {
     describe('with a non empty object', () => {
       it('should return the non empty object', () => {
