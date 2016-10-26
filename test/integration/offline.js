5445f2f93e601467148785bda2b605103d74d605
obearn <obearn@ekito.fr>
2016-10-26 14:58:57 +0200

    Add a OffLineBuilder to shorten test code. Add done callback for inject call back in mocha test.

--- test/integration/offline.js
+++ test/integration/offline.js
@@ -3,67 +3,99 @@
 const chai = require('chai');
 const dirtyChai = require('dirty-chai');
 const sinon = require('sinon');
-const functionHelper = require('../../src/functionHelper');
-const Offline = require('../../src/index');
 const ServerlessBuilder = require('../support/ServerlessBuilder');
+const OffLineBuilder = require('../support/OffLineBuilder');
 
 const expect = chai.expect;
 chai.use(dirtyChai);
 
 describe('Offline', () => {
-  const serverlessBuilder = new ServerlessBuilder();
-  serverlessBuilder.addFunction('fn1', {
-    handler: 'handler.hello',
-    events: [{
-      http: {
-        path: 'fn1',
-        method: 'GET',
-      },
-    }],
-  });
-  const serverless = serverlessBuilder.toObject();
+    let offline;
 
-  const options = {};
-
-  let offline;
-  let server;
-
-  before(() => {
-    offline = new Offline(serverless, options);
-    sinon.stub(offline, 'printBlankLine');
-    server = offline._buildServer();
-  });
-
-  context('with a non existing route', () => {
-    it('should return 404', () => {
-      const reqOpts = {
-        method: 'GET',
-        url: '/magic',
-      };
-
-      server.inject(reqOpts, (res) => {
-        expect(res.statusCode).to.eq(404);
-      });
+    before(() => {
+        //Creates offline test server with no function
+        offline = new OffLineBuilder(new ServerlessBuilder()).toObject();
     });
-  });
 
-  context('with an exiting lambda-proxy integration type route', () => {
-    it('should return the expected status code', () => {
-      const reqOpts = {
-        method: 'GET',
-        url: '/fn1',
-      };
-
-      sinon.stub(functionHelper, 'createHandler', () => () => ({
-        statusCode: 201,
-        body: null,
-      }));
-
-      server.inject(reqOpts, (res) => {
-        expect(res.statusCode).to.eq(201);
-      });
-
-      functionHelper.createHandler.restore();
+    context('with a non existing route', () => {
+        it('should return 404', () => {
+            offline.inject({
+                method: 'GET',
+                url: '/magic',
+            }, (res) => {
+                expect(res.statusCode).to.eq(404);
+            });
+        });
     });
-  });
+
+    context('with an exiting lambda-proxy integration type route', () => {
+        it('should return the expected status code', (done) => {
+            const offLine = new OffLineBuilder().addFunctionConfig('fn1', {
+                handler: 'handler.hello',
+                events: [{
+                    http: {
+                        path: 'fn1',
+                        method: 'GET',
+                    },
+                }],
+            }, (event, context, cb) => {cb(null, {
+                statusCode: 201,
+                body: null,
+            })}).toObject();
+
+            offLine.inject({
+                method: 'GET',
+                url: '/fn1',
+            }, (res) => {
+                expect(res.statusCode).to.eq(201);
+                done();
+            });
+        });
+    });
+
+    context('with an exiting lambda-proxy integration type route [SHORT MODE]', () => {
+        it('should return the expected status code', (done) => {
+            const offLine = new OffLineBuilder().addFunctionHTTP('hello', {
+                        path: 'fn1',
+                        method: 'GET'
+            }, (event, context, cb) => {cb(null, {
+                statusCode: 201,
+                body: null,
+            })}).toObject();
+
+            offLine.inject({
+                method: 'GET',
+                url: '/fn1',
+            }, (res) => {
+                expect(res.statusCode).to.eq(201);
+                done();
+            });
+        });
+    });
+
+    context('handling html', () => {
+        it.skip('should handle html end point configuration', (done) => {
+            const offLine = new OffLineBuilder().addFunctionConfig('index', {
+                handler: 'users.index',
+                events: [{
+                    //http: 'GET users/index'
+                    http: {
+                        path: 'index',
+                        method: 'GET',
+                        response: {
+                            headers: {
+                                'Content-Type': "'text/html'"
+                            },
+                            template: "$input.path('$')"
+                        }
+                    }
+                }]
+            }, (event, context, cb) => cb(null, 'Hello World')).toObject();
+
+            offLine.inject("/index", (res) => {
+                expect(res.statusCode).to.eq(201);
+                done();
+            });
+        });
+    });
 });
