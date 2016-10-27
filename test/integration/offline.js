84642b60a475e028b396e1e9dc060638efeafe0c
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2016-10-27 20:20:58 -0300

    linting and beautifying

--- test/integration/offline.js
+++ test/integration/offline.js
@@ -2,7 +2,6 @@
 
 const chai = require('chai');
 const dirtyChai = require('dirty-chai');
-const sinon = require('sinon');
 const ServerlessBuilder = require('../support/ServerlessBuilder');
 const OffLineBuilder = require('../support/OffLineBuilder');
 
@@ -10,95 +9,92 @@ const expect = chai.expect;
 chai.use(dirtyChai);
 
 describe('Offline', () => {
-    let offline;
+  let offline;
 
-    before(() => {
-        //Creates offline test server with no function
-        offline = new OffLineBuilder(new ServerlessBuilder()).toObject();
+  before(() => {
+    // Creates offline test server with no function
+    offline = new OffLineBuilder(new ServerlessBuilder()).toObject();
+  });
+
+  context('with a non existing route', () => {
+    it('should return 404', () => {
+      offline.inject({
+        method: 'GET',
+        url: '/magic',
+      }, (res) => {
+        expect(res.statusCode).to.eq(404);
+      });
     });
+  });
 
-    context('with a non existing route', () => {
-        it('should return 404', () => {
-            offline.inject({
-                method: 'GET',
-                url: '/magic',
-            }, (res) => {
-                expect(res.statusCode).to.eq(404);
-            });
-        });
+  context('with an exiting lambda-proxy integration type route', () => {
+    it('should return the expected status code', (done) => {
+      const offLine = new OffLineBuilder().addFunctionConfig('fn1', {
+        handler: 'handler.hello',
+        events: [{
+          http: {
+            path: 'fn1',
+            method: 'GET',
+          },
+        }],
+      }, (event, context, cb) => cb(null, {
+        statusCode: 201,
+        body: null,
+      })).toObject();
+
+      offLine.inject({
+        method: 'GET',
+        url: '/fn1',
+      }, (res) => {
+        expect(res.statusCode).to.eq(201);
+        done();
+      });
     });
+  });
 
-    context('with an exiting lambda-proxy integration type route', () => {
-        it('should return the expected status code', (done) => {
-            const offLine = new OffLineBuilder().addFunctionConfig('fn1', {
-                handler: 'handler.hello',
-                events: [{
-                    http: {
-                        path: 'fn1',
-                        method: 'GET',
-                    },
-                }],
-            }, (event, context, cb) => {cb(null, {
-                statusCode: 201,
-                body: null,
-            })}).toObject();
+  context('with an exiting lambda-proxy integration type route [SHORT MODE]', () => {
+    it('should return the expected status code', (done) => {
+      const offLine = new OffLineBuilder().addFunctionHTTP('hello', {
+        path: 'fn1',
+        method: 'GET',
+      }, (event, context, cb) => cb(null, {
+        statusCode: 201,
+        body: null,
+      })).toObject();
 
-            offLine.inject({
-                method: 'GET',
-                url: '/fn1',
-            }, (res) => {
-                expect(res.statusCode).to.eq(201);
-                done();
-            });
-        });
+      offLine.inject({
+        method: 'GET',
+        url: '/fn1',
+      }, (res) => {
+        expect(res.statusCode).to.eq(201);
+        done();
+      });
     });
+  });
 
-    context('with an exiting lambda-proxy integration type route [SHORT MODE]', () => {
-        it('should return the expected status code', (done) => {
-            const offLine = new OffLineBuilder().addFunctionHTTP('hello', {
-                        path: 'fn1',
-                        method: 'GET'
-            }, (event, context, cb) => {
-              console.log("fn");
-              return cb(null, {
-                statusCode: 201,
-                body: null,
-            });}).toObject();
+  context('handling html', () => {
+    it('should handle html end point configuration', (done) => {
+      const offLine = new OffLineBuilder().addFunctionConfig('index', {
+        handler: 'users.index',
+        events: [{
+          http: {
+            path: 'index',
+            method: 'GET',
+            integration: 'lambda',
+            response: {
+              headers: {
+                'Content-Type': "'text/html'",
+              },
+              template: "$input.path('$')",
+            },
+          },
+        }],
+      }, (event, context, cb) => cb(null, 'Hello World')).toObject();
 
-            offLine.inject({
-                method: 'GET',
-                url: '/fn1',
-            }, (res) => {
-                expect(res.statusCode).to.eq(201);
-                done();
-            });
-        });
+      offLine.inject('/index', (res) => {
+        expect(res.statusCode).to.eq('200');
+        done();
+      });
     });
-
-    context('handling html', () => {
-        it('should handle html end point configuration', (done) => {
-            const offLine = new OffLineBuilder().addFunctionConfig('index', {
-                handler: 'users.index',
-                events: [{
-                    //http: 'GET users/index'
-                    http: {
-                        path: 'index',
-                        method: 'GET',
-                        integration: 'lambda',
-                        response: {
-                            headers: {
-                                'Content-Type': "'text/html'"
-                            },
-                            template: "$input.path('$')"
-                        }
-                    }
-                }]
-            }, (event, context, cb) => cb(null, 'Hello World')).toObject();
-
-            offLine.inject("/index", (res) => {
-                expect(res.statusCode).to.eq('200');
-                done();
-            });
-        });
-    });
+  });
 });
--- test/support/OffLineBuilder.js
+++ test/support/OffLineBuilder.js
@@ -5,58 +5,59 @@ const functionHelper = require('../../src/functionHelper');
 const Offline = require('../../src/index');
 const ServiceBuilder = require('./ServerlessBuilder');
 
-
 function createHandler(handlers) {
-    return (funOptions) => {return handlers[funOptions.handlerPath.split('/')[1]][funOptions.handlerName]}
+  return (funOptions) => handlers[funOptions.handlerPath.split('/')[1]][funOptions.handlerName];
 }
 
 module.exports = class OffLineBuilder {
-    constructor(serviceBuilder, options) {
-        this.serviceBuilder = serviceBuilder || new ServiceBuilder();
-        this.handlers = {};
+  constructor(serviceBuilder, options) {
+    this.serviceBuilder = serviceBuilder || new ServiceBuilder();
+    this.handlers = {};
 
-        //Avoid already wrapped exception when offline is instanciated many times
-        //Problem if test are instanciated serveral times
-        //FIXME, we could refactor index to have an handlerFactory and just instanciate offline with a factory test stub
-        if (functionHelper.createHandler.restore) {
-            functionHelper.createHandler.restore();
-        }
-        this.options = options || {};
+    // Avoid already wrapped exception when offline is instanciated many times
+    // Problem if test are instanciated serveral times
+    // FIXME, we could refactor index to have an handlerFactory and just instanciate offline with a factory test stub
+    if (functionHelper.createHandler.restore) {
+      functionHelper.createHandler.restore();
     }
+    this.options = options || {};
+  }
 
-    addFunctionConfig(functionName, functionConfig, handler) {
-        this.serviceBuilder.addFunction(functionName, functionConfig);
-        const funOptions = functionHelper.getFunctionOptions(functionConfig, functionName, '.');
-        const handlerPath = funOptions.handlerPath.split('/')[1];
-        this.handlers[handlerPath] = this.constructor.getFunctionIndex(funOptions.handlerName, handler);
-        return this;
-    }
+  addFunctionConfig(functionName, functionConfig, handler) {
+    this.serviceBuilder.addFunction(functionName, functionConfig);
+    const funOptions = functionHelper.getFunctionOptions(functionConfig, functionName, '.');
+    const handlerPath = funOptions.handlerPath.split('/')[1];
+    this.handlers[handlerPath] = this.constructor.getFunctionIndex(funOptions.handlerName, handler);
+    return this;
+  }
 
-    addFunctionHTTP(functionName, http, handler) {
-        return this.addFunctionConfig(functionName, {
-            handler: `handler.${functionName}`,
-            events: [{
-                http
-            }],
-        }, handler);
-    }
+  addFunctionHTTP(functionName, http, handler) {
+    return this.addFunctionConfig(functionName, {
+      handler: `handler.${functionName}`,
+      events: [{
+        http,
+      }],
+    }, handler);
+  }
 
-    static getFunctionIndex(handlerName, handler) {
-        const functionIndex = {};
-        functionIndex[handlerName] = handler;
-        return functionIndex;
-    }
+  static getFunctionIndex(handlerName, handler) {
+    const functionIndex = {};
+    functionIndex[handlerName] = handler;
+    return functionIndex;
+  }
 
-    toObject() {
-        const offline = new Offline(this.serviceBuilder.toObject(), this.options);
-        sinon.stub(functionHelper, 'createHandler', createHandler(this.handlers));
-        sinon.stub(offline, 'printBlankLine');
-        this.server = offline._buildServer();
-        Object.assign(this.server, {restore: this.restore});
-        return this.server;
-    }
+  toObject() {
+    const offline = new Offline(this.serviceBuilder.toObject(), this.options);
+    sinon.stub(functionHelper, 'createHandler', createHandler(this.handlers));
+    sinon.stub(offline, 'printBlankLine');
+    this.server = offline._buildServer();
+    Object.assign(this.server, {
+      restore: this.restore,
+    });
+    return this.server;
+  }
 
-    static restore() {
-        functionHelper.createHandler.restore();
-    }
+  static restore() {
+    functionHelper.createHandler.restore();
+  }
 };
