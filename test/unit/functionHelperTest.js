b2bc36488b09da7f89ef68e7a5896878c3a44d40
emmoistner <emmoistner@gmail.com>
2017-03-08 18:57:04 -0500

    add createHandler tests

--- test/unit/functionHelperTest.js
+++ test/unit/functionHelperTest.js
@@ -4,10 +4,13 @@
 
 const chai = require('chai');
 const dirtyChai = require('dirty-chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
 const functionHelper = require('../../src/functionHelper');
 
 const expect = chai.expect;
 chai.use(dirtyChai);
+chai.use(sinonChai);
 
 describe('functionHelper', () => {
   describe('#getFunctionOptions', () => {
@@ -56,5 +59,49 @@ describe('functionHelper', () => {
         expect(result.funTimeout).to.eql(7000);
       });
     });
+  });
+
+  describe('#createHandler', () => {
+
+    function stubHandler() { }
+
+    const funOptions = {
+      handlerName: 'hello',
+      handlerPath: '/handler',
+    };
+
+    before(() => {
+      // Somebody isn't being a very good citizen
+      if (functionHelper.createHandler.restore) {
+        functionHelper.createHandler.restore();
+      }
+    });
+
+    it('should load a module handler with handlerLoader', () => {
+      const options = { verboseHandlerLoader: false };
+      const handlerLoader = functionHelper.handlerLoader;
+
+      // set handlerLoader to a dummy spy
+      functionHelper.handlerLoader = sinon.spy(() => stubHandler);
+      const handler = functionHelper.createHandler(funOptions, options);
+
+      expect(functionHelper.handlerLoader).to.be.have.calledWith(funOptions);
+      expect(handler).to.eq(stubHandler);
+      functionHelper.handlerLoader = handlerLoader;
+    });
+
+    it('it should load a module handler with verboseHandlerLoader', () => {
+      const options = { verboseHandlerLoader: true };
+      const verboseHandlerLoader = functionHelper.verboseHandlerLoader;
+
+      // set verboseHandlerLoader to a dummy spy
+      functionHelper.verboseHandlerLoader = sinon.spy(() => stubHandler);
+      const handler = functionHelper.createHandler(funOptions, options);
+
+      expect(functionHelper.verboseHandlerLoader).to.be.have.calledWith(funOptions);
+      expect(handler).to.eq(stubHandler);
+      functionHelper.verboseHandlerLoader = verboseHandlerLoader;
+    });
+
   });
 });
