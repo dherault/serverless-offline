58ea854bf33c0bc78b6471aacf1a1e986d5b0465
emmoistner <emmoistner@gmail.com>
2017-03-08 17:18:51 -0500

    add verboseHandlerLoader option

--- src/functionHelper.js
+++ src/functionHelper.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const debugLog = require('./debugLog');
+const path = require('path');
+const execSync = require('child_process').execSync;
 
 module.exports = {
   getFunctionOptions(fun, funName, servicePath) {
@@ -31,8 +33,7 @@ module.exports = {
       }
     }
 
-    debugLog(`Loading handler... (${funOptions.handlerPath})`);
-    const handler = require(funOptions.handlerPath)[funOptions.handlerName];
+    const handler = options.verboseHandlerLoader ? this.verboseHandlerLoader(funOptions) : this.handlerLoader(funOptions);
 
     if (typeof handler !== 'function') {
       throw new Error(`Serverless-offline: handler for '${funOptions.funName}' is not a function`);
@@ -40,4 +41,54 @@ module.exports = {
 
     return handler;
   },
+
+  // require module/handler from handlerPath
+  handlerLoader(funOptions) {
+    debugLog(`Loading handler... (${funOptions.handlerPath})`);
+
+    return require(funOptions.handlerPath)[funOptions.handlerName];
+  },
+
+  // Verbose Load lambda handler and better loading errors
+  // Attempts to load handlerPath
+  // On failure, it execs the handleFile and throw interpreter errors
+  // This is workaround for node 4.x, should be fixed in 6+
+  // https://github.com/nodejs/node/issues/2762
+  verboseHandlerLoader(funOptions) {
+    debugLog(`Loading handler (verbose)... (${funOptions.handlerPath})`);
+
+    let moduleLoadError,
+      handler;
+
+    // Basic to attempt to load handler
+    try {
+      handler = require(funOptions.handlerPath)[funOptions.handlerName];
+    }
+    catch (error) {
+      moduleLoadError = error;
+    }
+
+    // Let's try to get a better, more in depth error stack
+    if (moduleLoadError) {
+      try {
+        const ext = path.extname(funOptions.handlerPath);
+        execSync(`node ${funOptions.handlerPath}${ext ? '' : '.js'}`, { stdio: [null, null] });
+      }
+      catch (execError) {
+        // remove internal exec failed message (useless trace to Offline users)
+        const stack = execError.stack.split('\n');
+        stack.shift();
+        execError.stack = stack.join('\n');
+
+        throw execError;
+      }
+
+      // Unable to find deeper error from manually loading
+      // throwing require error
+      throw moduleLoadError;
+    }
+
+    return handler;
+  },
+
 };
