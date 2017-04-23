d8fd826da27ccf8b7ca4e6c3cd71e9927d05cb19
Garun Vagidov <garun@garunski.com>
2017-04-23 16:42:19 -0700

    Added ignore the authorizer for the non custom authorizers.
    The endpoints still work as is even tho the authorizer is not supported.
    Added manual tests for the ARN authorizer and custom function
    authorizer.

--- manual_test/handler.js
+++ manual_test/handler.js
@@ -12,6 +12,23 @@ module.exports.hello = (event, context, callback) => {
   callback(null, response);
 };
 
+module.exports.authFunction = (event, context, callback) => {
+  context.succeed({
+    principalId: 'xxxxxxx', // the principal user identification associated with the token send by the client
+    policyDocument: {
+      // example policy shown below, but this value is any valid policy
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Effect: 'Allow',
+          Action: ['execute-api:Invoke'],
+          Resource: [event.methodArn],
+        },
+      ],
+    },
+  });
+};
+
 module.exports.hello500 = (event, context, callback) => {
   const response = {
     statusCode: 500,
