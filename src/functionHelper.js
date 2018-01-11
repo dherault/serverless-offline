f471ebadd10413d23dcb30c79e8bf12e0963c9a6
Dan Bovey <dan@danbovey.uk>
2018-01-11 14:33:29 +0000

    Separate python output such as prints or errors from JSON output

--- src/functionHelper.js
+++ src/functionHelper.js
@@ -2,78 +2,85 @@
 
 const debugLog = require('./debugLog');
 
-function runPythonHandler(funOptions, options){
+function runPythonHandler(funOptions, options) {
     var spawn = require("child_process").spawn;
-      return function(event,context){
-      var process = spawn('sls',["invoke", "local", "-f", funOptions.funName],
-          {stdio: ['pipe', 'pipe', 'pipe'], shell: true, cwd:funOptions.servicePath});
-      process.stdin.write(JSON.stringify(event)+"\n");
-      process.stdin.end();
-      let results = ''
-      process.stdout.on('data', (data) => {
-         results = results + data;
-      });
-      process.stderr.on('data', (data) => {
-         context.fail(data);
-      });
-      process.on('close', (code) => {
-          if (code == 0) {
-              try {
-                context.succeed( JSON.parse(results) );
-              } catch (ex) {
-                context.fail(results);
-              }
-          } else {
-              context.succeed( code ,results );
-          }  
+    return function (event, context) {
+        var process = spawn('sls', ["invoke", "local", "-f", funOptions.funName],
+            { stdio: ['pipe', 'pipe', 'pipe'], shell: true, cwd: funOptions.servicePath });
+        process.stdin.write(JSON.stringify(event) + "\n");
+        process.stdin.end();
+        let results = ''
+        process.stdout.on('data', (data) => {
+            try {
+                JSON.parse(data);
+                // The output is JSON
+                results = results + data;
+            } catch(ex) {
+                // The data is Write the data to the console instead
+                console.log('Python:', '\x1b[34m' + data.toString('utf8') + '\x1b[0m');
+            }
         });
-      }
+        process.stderr.on('data', (data) => {
+            context.fail(data);
+        });
+        process.on('close', (code) => {
+            if (code == 0) {
+                try {
+                    context.succeed(JSON.parse(results));
+                } catch (ex) {
+                    context.fail(results);
+                }
+            } else {
+                context.succeed(code, results);
+            }
+        });
+    }
 }
 
 module.exports = {
-  getFunctionOptions(fun, funName, servicePath,serviceRuntime) {
-    console.log(fun, funName, servicePath)
-    // Split handler into method name and path i.e. handler.run
-    const handlerFile = fun.handler.split('.')[0];
-    const handlerName = fun.handler.split('/').pop().split('.')[1];
+    getFunctionOptions(fun, funName, servicePath, serviceRuntime) {
+        console.log(fun, funName, servicePath)
+        // Split handler into method name and path i.e. handler.run
+        const handlerFile = fun.handler.split('.')[0];
+        const handlerName = fun.handler.split('/').pop().split('.')[1];
 
-    return {
-      funName,
-      handlerName, // i.e. run
-      handlerFile,
-      handlerPath: `${servicePath}/${handlerFile}`,
-      servicePath,
-      funTimeout: (fun.timeout || 30) * 1000,
-      babelOptions: ((fun.custom || {}).runtime || {}).babel,
-      serviceRuntime,
-    };
-  },
+        return {
+            funName,
+            handlerName, // i.e. run
+            handlerFile,
+            handlerPath: `${servicePath}/${handlerFile}`,
+            servicePath,
+            funTimeout: (fun.timeout || 30) * 1000,
+            babelOptions: ((fun.custom || {}).runtime || {}).babel,
+            serviceRuntime,
+        };
+    },
 
-  
-  // Create a function handler
-  // The function handler is used to simulate Lambda functions
-  createHandler(funOptions, options) {
-    if (!options.skipCacheInvalidation) {
-      debugLog('Invalidating cache...');
 
-      for (const key in require.cache) {
-        // Require cache invalidation, brutal and fragile.
-        // Might cause errors, if so please submit an issue.
-        if (!key.match('node_modules')) delete require.cache[key];
-      }
-    }
-    let user_python = true
-    let handler = null;
-    if (['python2.7', 'python3.6'].indexOf(funOptions['serviceRuntime']) !== -1){
-      handler = runPythonHandler(funOptions, options)
-    } else {
-      debugLog(`Loading handler... (${funOptions.handlerPath})`);
-      handler = require(funOptions.handlerPath)[funOptions.handlerName];
-    }
-    if (typeof handler !== 'function') {
-      throw new Error(`Serverless-offline: handler for '${funOptions.funName}' is not a function`);
-    }
+    // Create a function handler
+    // The function handler is used to simulate Lambda functions
+    createHandler(funOptions, options) {
+        if (!options.skipCacheInvalidation) {
+            debugLog('Invalidating cache...');
 
-    return handler;
-  },
+            for (const key in require.cache) {
+                // Require cache invalidation, brutal and fragile.
+                // Might cause errors, if so please submit an issue.
+                if (!key.match('node_modules')) delete require.cache[key];
+            }
+        }
+        let user_python = true
+        let handler = null;
+        if (['python2.7', 'python3.6'].indexOf(funOptions['serviceRuntime']) !== -1) {
+            handler = runPythonHandler(funOptions, options)
+        } else {
+            debugLog(`Loading handler... (${funOptions.handlerPath})`);
+            handler = require(funOptions.handlerPath)[funOptions.handlerName];
+        }
+        if (typeof handler !== 'function') {
+            throw new Error(`Serverless-offline: handler for '${funOptions.funName}' is not a function`);
+        }
+
+        return handler;
+    },
 };
