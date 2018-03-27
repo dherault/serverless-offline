4162446f8b8626c40b6bb3dc915a3a0908dbc9ca
Tiago de Assis Gonçalves <contato@tiagogoncalves.com.br>
2018-03-27 11:34:00 -0300

    Added support to noEnvironment flag (#367)
    * Added support for noEnvironment flag, serverless variables dont will be replaced by machine variables and avoid aws services like ssm to throw error when up the server
    
    * Released 3.19 version
    
    * now, environment variables replace serverless variables

--- src/index.js
+++ src/index.js
@@ -496,7 +496,15 @@ class Offline {
             let handler; // The lambda function
 
             try {
-              process.env = _.extend({}, this.service.provider.environment, this.service.functions[key].environment, this.originalEnvironment);
+              // This evict errors in server when we use aws services like ssm
+              const baseEnvironment = {
+                AWS_ACCESS_KEY_ID: 'dev',
+                AWS_SECRET_ACCESS_KEY: 'dev',
+                AWS_REGION: 'dev'
+              }
+              process.env = _.extend({}, baseEnvironment);
+              if (!this.options.noEnvironment) Object.assign(process.env, this.service.provider.environment, this.service.functions[key].environment)
+              Object.assign(process.env, this.originalEnvironment)
               handler = functionHelper.createHandler(funOptions, this.options);
             }
             catch (err) {
