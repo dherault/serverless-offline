e8ff44a1be81013ce54c0c199a4b2d4523fc42ef
David Hérault <dherault@gmail.com>
2019-01-12 11:34:18 +0100

    remove more of lodash

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@ module.exports = {
   toPlainOrEmptyObject: obj => typeof obj === 'object' && !Array.isArray(obj) ? obj : {},
   randomId: () => Math.random().toString(10).slice(2),
   nullIfEmpty: o => o && (Object.keys(o).length > 0 ? o : null),
+  isPlainObject: obj => typeof obj === 'object' && !Array.isArray(obj),
   normalizeQuery: query =>
     // foreach key, get the last element if it's an array
     Object.keys(query).reduce((q, param) => {
