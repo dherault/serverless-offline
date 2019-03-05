acefeb7cda7dac891e12ccecfda69c50b1cfebe1
Andreas Zoellner <andreaszoellner@gmail.com>
2019-03-05 10:37:06 -0800

    reorder one test

--- test/unit/authMatchPolicyResourceTest.js
+++ test/unit/authMatchPolicyResourceTest.js
@@ -77,7 +77,24 @@ describe('authMatchPolicyResource', () => {
 
       context('when the resource has multiple wildcards', () => {
         const wildcardResource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/*/*/stats';
+        context('and it matches', () => {
+          it('returns true', () => {
+            const resource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/GET/dinosaurs/stats';
 
+            expect(
+              authMatchPolicyResource(wildcardResource, resource)
+            ).to.eq(true);
+          });
+        });
+        context('and it does not match', () => {
+          it('returns false', () => {
+            const resource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/PUT/dinosaurs/xx';
+
+            expect(
+              authMatchPolicyResource(wildcardResource, resource)
+            ).to.eq(false);
+          });
+        });
         context('and the wildcard is between two fragments', () => {
           const wildcardResource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/*/dinosaurs/*';
           context('and it matches', () => {
@@ -97,24 +114,6 @@ describe('authMatchPolicyResource', () => {
                 authMatchPolicyResource(wildcardResource, resource)
               ).to.eq(false);
             });
-          });
-        });
-        context('and it matches', () => {
-          it('returns true', () => {
-            const resource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/GET/dinosaurs/stats';
-
-            expect(
-              authMatchPolicyResource(wildcardResource, resource)
-            ).to.eq(true);
-          });
-        });
-        context('and it does not match', () => {
-          it('returns false', () => {
-            const resource = 'arn:aws:execute-api:eu-west-1:random-account-id:random-api-id/development/PUT/dinosaurs/xx';
-
-            expect(
-              authMatchPolicyResource(wildcardResource, resource)
-            ).to.eq(false);
           });
         });
       });
