f82235948df1c9e191d1b19ea975269685455e3f
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-04-19 22:59:50 -0400

    Move require child_process to top, combine requiring fork and spawn.

--- src/functionHelper.js
+++ src/functionHelper.js
@@ -1,8 +1,6 @@
-/* eslint-disable import/no-dynamic-require */
-const trimNewlines = require('trim-newlines');
-const fork = require('child_process').fork;
+const { fork, spawn } = require('child_process');
 const path = require('path');
-
+const trimNewlines = require('trim-newlines');
 const debugLog = require('./debugLog');
 const utils = require('./utils');
 
@@ -10,8 +8,6 @@ const handlerCache = {};
 const messageCallbacks = {};
 
 function runProxyHandler(funOptions, options) {
-  const spawn = require('child_process').spawn;
-
   return (event, context) => {
     const args = ['invoke', 'local', '-f', funOptions.funName];
     const stage = options.s || options.stage;
