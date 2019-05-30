92511e31a0479ec82fce317b0b53e80d2eed44e8
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-05-29 20:44:49 -0400

    Use module constant for function timeout, increase to 900 sec (15 min)

--- src/createLambdaContext.js
+++ src/createLambdaContext.js
@@ -1,5 +1,9 @@
 const utils = require('./utils');
 
+// https://docs.aws.amazon.com/lambda/latest/dg/limits.html
+// default function timeout in seconds
+const DEFAULT_TIMEOUT = 900; // 15 min
+
 /*
   Mimicks the lambda context object
   http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
@@ -7,7 +11,7 @@ const utils = require('./utils');
 module.exports = function createLambdaContext(fun, provider, cb) {
 
   const functionName = fun.name;
-  const timeout = (fun.timeout || provider.timeout || 6) * 1000; // default 6 second timeout
+  const timeout = (fun.timeout || provider.timeout || DEFAULT_TIMEOUT) * 1000;
   const endTime = new Date().getTime() + timeout;
 
   return {
