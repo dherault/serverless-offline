71dd3cbb35dffac4edcb662369f433fa693a0125
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-03 15:17:43 -0400

    Order props and methods on lambda context, add comment regarding prototype

--- src/createLambdaContext.js
+++ src/createLambdaContext.js
@@ -9,27 +9,29 @@ const DEFAULT_TIMEOUT = 900; // 15 min
   http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
 */
 module.exports = function createLambdaContext(fun, provider, cb) {
-
   const functionName = fun.name;
   const timeout = (fun.timeout || provider.timeout || DEFAULT_TIMEOUT) * 1000;
   const endTime = new Date().getTime() + timeout;
 
   return {
-    /* Methods */
+    // doc-deprecated methods
     done: cb,
     succeed: res => cb(null, res, true),
     fail: err => cb(err, null, true),
+
+    // methods
+    // NOTE: the AWS context methods are OWN FUNCTIONS (NOT on the prototype!)
     getRemainingTimeInMillis: () => endTime - new Date().getTime(),
 
-    /* Properties */
+    // properties
+    awsRequestId: `offline_awsRequestId_${randomId()}`,
+    clientContext: {},
     functionName,
-    memoryLimitInMB:    fun.memorySize || provider.memorySize,
-    functionVersion:    `offline_functionVersion_for_${functionName}`,
+    functionVersion: `offline_functionVersion_for_${functionName}`,
+    identity: {},
     invokedFunctionArn: `offline_invokedFunctionArn_for_${functionName}`,
-    awsRequestId:       `offline_awsRequestId_${randomId()}`,
-    logGroupName:       `offline_logGroupName_for_${functionName}`,
-    logStreamName:      `offline_logStreamName_for_${functionName}`,
-    identity:           {},
-    clientContext:      {},
+    logGroupName: `offline_logGroupName_for_${functionName}`,
+    logStreamName: `offline_logStreamName_for_${functionName}`,
+    memoryLimitInMB: fun.memorySize || provider.memorySize,
   };
 };
