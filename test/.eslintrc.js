43e581ee4750f69234f2600e84b97a87cae37c28
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-06-05 11:57:32 -0400

    Add additional tests

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,6 +1,7 @@
 
 const rules = {
   // https://github.com/eslint/eslint/issues/2102
+  'arrow-body-style': 'off',
   'no-unused-expressions': 'off',
 };
 
--- test/integration/handler.test.js
+++ test/integration/handler.test.js
@@ -24,11 +24,14 @@ describe('handler tests', () => {
       ],
       [
         'when handler returns rejected Promise',
-        // eslint-disable-next-line arrow-body-style
-        () => {
-          // NOTE: return promise explicitly!
-          return Promise.reject(new Error('foo'));
-        },
+        () => Promise.reject(new Error('foo')),
+      ],
+      [
+        'when handler that defers returns rejected Promise',
+        () =>
+          new Promise((resolve, reject) => {
+            setTimeout(() => reject(new Error('foo')), 10);
+          }),
       ],
       [
         'when async function handler throws',
@@ -43,15 +46,39 @@ describe('handler tests', () => {
         },
       ],
       [
+        'when callback that defers returns error',
+        (event, context, callback) => {
+          setTimeout(() => {
+            callback(new Error('foo'));
+          }, 10);
+        },
+      ],
+      [
         'when context.done returns error',
         (event, context) => {
           context.done(new Error('foo'));
         },
       ],
       [
+        'when context.done that defers returns error',
+        (event, context) => {
+          setTimeout(() => {
+            context.done(new Error('foo'));
+          }, 10);
+        },
+      ],
+      [
         'when context.fail returns error',
         (event, context) => {
           context.fail(new Error('foo'));
+        },
+      ],
+      [
+        'when context.fail that defers returns error',
+        (event, context) => {
+          setTimeout(() => {
+            context.fail(new Error('foo'));
+          }, 10);
         },
       ],
     ].forEach(([description, handler]) => {
@@ -80,6 +107,136 @@ describe('handler tests', () => {
           .to.have.property('content-type')
           .which.contains('application/json');
         expect(response.statusCode).to.eq(502);
+        expect(parse(response.payload)).to.deep.equal(expectedResponse);
+      });
+    });
+  });
+
+  // NOTE: mix and matching of callbacks and promises is not recommended,
+  // nonetheless, we test some of the behaviour to match AWS execution precedence
+  describe('execution precedence)', () => {
+    [
+      {
+        // we deliberately test the case where a 'callback' is defined
+        // in the handler, but a promise is being returned to protect from a
+        // potential naive implementation, e.g.
+        //
+        // const { promisify } = 'utils'
+        // const promisifiedHandler = handler.length === 3 ? promisify(handler) : handler
+        //
+        // if someone would return a promise, but also defines callback, without using it
+        // the handler would not be returning anything
+        description: '...',
+        // eslint-disable-next-line no-unused-vars
+        handler: (event, context, callback) => {
+          return Promise.resolve({
+            statusCode: 200,
+            message: JSON.stringify({ message: 'Hello Promise' }),
+          });
+        },
+        expectedResponse: {
+          message: 'Hello Context.succeed',
+        },
+      },
+
+      {
+        description: '...',
+        handler: (event, context) => {
+          context.succeed({
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello Context.succeed' }),
+          });
+
+          context.done({
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello Context.done' }),
+          });
+        },
+        expectedResponse: {
+          message: 'Hello Context.succeed',
+        },
+      },
+
+      {
+        description: '...',
+        handler: (event, context, callback) => {
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello Callback' }),
+          });
+
+          context.done(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello Context.done' }),
+          });
+        },
+        expectedResponse: {
+          message: 'Hello Callback',
+        },
+      },
+
+      {
+        description: '...',
+        handler: (event, context, callback) => {
+          callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Hello Callback' }),
+          });
+
+          return Promise.resolve({
+            statusCode: 200,
+            message: JSON.stringify({ message: 'Hello Promise' }),
+          });
+        },
+        expectedResponse: {
+          message: 'Hello Callback',
+        },
+      },
+
+      {
+        description: '...',
+        handler: (event, context, callback) => {
+          return new Promise(resolve => {
+            callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({ message: 'Hello Callback' }),
+            });
+
+            resolve({
+              statusCode: 200,
+              body: JSON.stringify({ message: 'Hello Promise' }),
+            });
+          });
+        },
+        expectedResponse: {
+          message: 'Hello Callback',
+        },
+      },
+
+    ].forEach(testCase => {
+      const { description, handler, expectedResponse } = testCase;
+
+      it(description, async () => {
+        const offline = new OfflineBuilder(new ServerlessBuilder())
+          .addFunctionHTTP(
+            'index',
+            {
+              method: 'GET',
+              path: 'index',
+            },
+            handler,
+          )
+          .toObject();
+
+        const response = await offline.inject({
+          method: 'GET',
+          url: '/index',
+        });
+
+        expect(response.headers)
+          .to.have.property('content-type')
+          .which.contains('application/json');
+        expect(response.statusCode).to.eq(200);
         expect(parse(response.payload)).to.deep.equal(expectedResponse);
       });
     });
