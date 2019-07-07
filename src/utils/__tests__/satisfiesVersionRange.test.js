02018a45607810ad93af2fc94c4c1cd888ac4415
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-07-07 14:09:19 -0400

    Switch test parameter positions

--- src/utils/__tests__/satisfiesVersionRange.test.js
+++ src/utils/__tests__/satisfiesVersionRange.test.js
@@ -5,10 +5,10 @@ const satisfiesVersionRange = require('../satisfiesVersionRange.js');
 describe('satisfiesVersionRange', () => {
   describe('valid parameters', () => {
     test.each`
-      version     | range       | expected | description
-      ${'1.38.0'} | ${'>=1.38'} | ${true}  | ${'same as minimum'}
-      ${'1.40.0'} | ${'>=1.38'} | ${true}  | ${'greather than minimum'}
-      ${'1.0.0'}  | ${'>=1.38'} | ${false} | ${'less than minimum'}
+      range       | version      | expected | description
+      ${'>=1.38'} | ${'1.38.0'}  | ${true}  | ${'same as minimum'}
+      ${'>=1.38'} | ${'1.40.0'}  | ${true}  | ${'greather than minimum'}
+      ${'>=1.38'} | ${'1.37.11'} | ${false} | ${'less than minimum'}
     `(
       'should return $expected when $description is passed',
       ({ expected, range, version }) => {
@@ -20,9 +20,9 @@ describe('satisfiesVersionRange', () => {
 
   describe('should throw when invalid parameters are passed', () => {
     test.each`
-      version     | range       | description
-      ${'a.b.c'}  | ${'>=1.40'} | ${'invalid version'}
-      ${'1.40.0'} | ${'a.b.c'}  | ${'invalid range'}
+      range       | version     | description
+      ${'>=1.40'} | ${'a.b.c'}  | ${'invalid version'}
+      ${'a.b.c'}  | ${'1.40.0'} | ${'invalid range'}
     `(
       'should throw error when $description is passed',
       ({ range, version }) => {
