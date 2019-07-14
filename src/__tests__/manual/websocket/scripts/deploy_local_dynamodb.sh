b56b8c5dd8106676ad860f83399f5480cf6aaee3
@computerpunc <computerpunc@gmail.com>
2019-07-14 16:03:22 +0300

    Fix - doesn't  install  dynamodb before deploying  scheme

--- src/__tests__/manual/websocket/scripts/deploy_local_dynamodb.sh
+++ src/__tests__/manual/websocket/scripts/deploy_local_dynamodb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo "Deploying Local DynamoDB:" 
+echo "Installing Local DynamoDB ..."
+sls dynamodb install --config ./config/serverless.data.yml
+echo "Installing Local Scheme ..."
 sls dynamodb start --config ./config/serverless.data.yml
