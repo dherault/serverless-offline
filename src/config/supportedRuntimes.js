88fecc41e5cb6e7a56ce00dbffac0ef6e7389570
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-07-25 13:21:27 -0400

    Add deprecated/unsupported runtimes (still working)

--- src/config/supportedRuntimes.js
+++ src/config/supportedRuntimes.js
@@ -3,29 +3,62 @@
 // native runtime support for AWS
 // https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html
 
+// deprecated runtimes
+// https://docs.aws.amazon.com/lambda/latest/dg/runtime-support-policy.html
 module.exports = new Set([
-  // .net
-  // dotnetcore2.1
-  // dotnetcore1.0
+  // ==========
+  // .NET CORE
+  // ==========
 
-  // go
+  // deprecated
+  // 'dotnetcore1.0',
+  // 'dotnetcore2.0',
+
+  // supported
+  // dotnetcore2.1
+
+  // ==========
+  // GO
+  // ==========
+
   'go1.x',
 
-  // java
+  // ==========
+  // JAVA
+  // ==========
+
+  // supported
   // java8
 
-  // node.js
+  // ==========
+  // NODE.JS
+  // ==========
+
+  // deprecated, but still working
+  'nodejs4.3',
+  'nodejs6.10',
+
+  // supported
   'nodejs8.10',
   'nodejs10.x',
 
-  // provided
+  // ==========
+  // PROVIDED
+  // ==========
+
   'provided',
 
-  // python
+  // ==========
+  // PYTHON
+  // ==========
+
   'python2.7',
   'python3.6',
   'python3.7',
 
-  // ruby
+  // ==========
+  // RUBY
+  // ==========
+
   'ruby2.5',
 ]);
