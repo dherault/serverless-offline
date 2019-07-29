3d26a10114324782cc94b9c18a475065cc5900c6
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-07-28 20:59:06 -0400

    Add python integration test [skipped]

---
+++ src/__tests__/integration/python/pythonHandler.test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { resolve } = require('path');
+const { URL } = require('url');
+const fetch = require('node-fetch');
+const Serverless = require('serverless');
+const ServerlessOffline = require('../../../../src/ServerlessOffline.js');
+
+jest.setTimeout(10000);
+
+describe.skip('python tests', () => {
+  let serverlessOffline;
+
+  // init
+  beforeAll(async () => {
+    const serverless = new Serverless();
+    serverless.config.servicePath = resolve(__dirname);
+    await serverless.init();
+    serverlessOffline = new ServerlessOffline(serverless, {});
+
+    return serverlessOffline.start();
+  });
+
+  // cleanup
+  afterAll(async () => {
+    return serverlessOffline.end();
+  });
+
+  const url = new URL('http://localhost:3000');
+
+  [
+    {
+      description: 'should work with python',
+      expected: {
+        message: 'Hello Python!',
+      },
+      path: 'hello',
+    },
+  ].forEach(({ description, expected, path }) => {
+    test(description, async () => {
+      url.pathname = path;
+      const response = await fetch(url);
+      const json = await response.json();
+      expect(json).toEqual(expected);
+    });
+  });
+});
