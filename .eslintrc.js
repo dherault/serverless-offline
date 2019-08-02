601a6933a01d23aca262aa47b08ab66753dfcb21
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-01 21:37:14 -0400

    Move eslint config rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,6 @@
 
 const { env, platform } = process;
 
-const rules = {
-  'guard-for-in': 'off',
-  'import/no-dynamic-require': 'off',
-  'key-spacing': 'off',
-  'no-restricted-syntax': 'off',
-  'one-var-declaration-per-line': ['error', 'always'],
-  semi: ['error', 'always'],
-  strict: 'off',
-};
-
-// workaround for git + eslint line ending issue on Travis for Windows OS:
-// https://travis-ci.community/t/files-in-checkout-have-eol-changed-from-lf-to-crlf/349/2
-if (env.TRAVIS && platform === 'win32') {
-  rules['linebreak-style'] = 'off';
-}
-
 module.exports = {
   extends: ['eslint:recommended', 'dherault', 'eslint-config-prettier'],
 
@@ -29,5 +13,19 @@ module.exports = {
     node: true,
   },
 
-  rules,
+  rules: {
+    'guard-for-in': 'off',
+    'import/no-dynamic-require': 'off',
+    'key-spacing': 'off',
+    'no-restricted-syntax': 'off',
+    'one-var-declaration-per-line': ['error', 'always'],
+    semi: ['error', 'always'],
+    strict: 'off',
+    // workaround for git + eslint line ending issue on Travis for Windows OS:
+    // https://travis-ci.community/t/files-in-checkout-have-eol-changed-from-lf-to-crlf/349/2
+    ...(env.TRAVIS &&
+      platform === 'win32' && {
+        ['linebreak-style']: 'off',
+      }),
+  },
 };
