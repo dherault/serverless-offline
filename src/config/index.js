2116ddb2b72bfffc1215c06ce77a8d708101fdbe
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-02 11:00:29 -0400

    Remove space

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,6 @@
 'use strict';
 
 module.exports = require('./constants.js');
-
 module.exports.defaults = require('./defaults.js');
-
 module.exports.options = require('./options.js');
-
 module.exports.supportedRuntimes = require('./supportedRuntimes.js');
