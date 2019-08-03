fee2f7da57a52637d3b6f34a48268dc4f2cdba2d
@computerpunc <computerpunc@gmail.com>
2019-08-03 08:19:47 +0300

    When running 'deploy-dynamodb-local', fix no 'serverless-offline' installed.

--- src/__tests__/manual/websocket/scripts/deploy_local_dynamodb.sh
+++ src/__tests__/manual/websocket/scripts/deploy_local_dynamodb.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 echo "Deploying Local DynamoDB:" 
+if [ -d "./node_modules/serverless-offline" ]; then 
+  if [ ! -L "./node_modules/serverless-offline" ]; then
+    echo "Removing serverless-offline ..."
+    npm remove serverless-offline
+    echo "Linking serverless-offline ..."
+    npm link serverless-offline
+  else 
+    echo "serverless-offline already linked."
+  fi
+else
+  echo "Linking serverless-offline ..."
+  npm link serverless-offline
+fi
 echo "Installing Local DynamoDB ..."
 sls dynamodb install --config ./config/serverless.data.yml
 echo "Installing Local Scheme ..."
