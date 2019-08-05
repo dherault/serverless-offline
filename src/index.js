50182832fea85b4020005b02a240360aa8b2e9fb
Mariusz Nowak <mariusz@medikoo.com>
2019-08-05 13:21:26 +0200

    Do not remove unhandledRejection listeners

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 'use strict';
 
 module.exports = require('./ServerlessOffline.js');
-
-// Serverless exits with code 1 when a promise rejection is unhandled. Not AWS.
-// Users can still use their own unhandledRejection event though.
-process.removeAllListeners('unhandledRejection');
