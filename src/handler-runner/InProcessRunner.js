cc484542ca9c30b802f57edaf764f3f727804aa4
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-17 13:47:17 -0400

    Fix handler module path check, was broken for module not found in handler

--- src/handler-runner/InProcessRunner.js
+++ src/handler-runner/InProcessRunner.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const serverlessLog = require('../serverlessLog.js')
-
 module.exports = class InProcessRunner {
   constructor(functionName, handlerPath, handlerName) {
     this._functionName = functionName
@@ -10,22 +8,15 @@ module.exports = class InProcessRunner {
   }
 
   run(event, context, callback) {
-    // TODO error handling? when module and/or function is not found
-    // lazy load handler with first usage
-
-    let handler
-
-    try {
-      handler = require(this._handlerPath)[this._handlerName] // eslint-disable-line
-    } catch (err) {
-      if (err.code === 'MODULE_NOT_FOUND') {
-        serverlessLog.errorLog(
-          `Could not find handler module '${this._handlerPath}' for function '${this._functionName}'.`,
-        )
-      }
-
-      throw err
+    // check if the handler module path exists
+    if (!require.resolve(this._handlerPath)) {
+      throw new Error(
+        `Could not find handler module '${this._handlerPath}' for function '${this._functionName}'.`,
+      )
     }
+
+    // lazy load handler with first usage
+    const handler = require(this._handlerPath)[this._handlerName] // eslint-disable-line
 
     if (typeof handler !== 'function') {
       throw new Error(
