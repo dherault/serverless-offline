68fcb8323bb31e1b03b1428a328f6bf475a654e7
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-22 09:21:03 -0400

    Re-introduce invoking ruby handlers

--- src/handler-runner/ServerlessInvokeLocalRunner.js
+++ src/handler-runner/ServerlessInvokeLocalRunner.js
@@ -9,9 +9,28 @@ const { cwd, env } = process
 
 module.exports = class ServerlessInvokeLocalRunner {
   constructor(funOptions, stage) {
+    const { handlerName, handlerPath, runtime } = funOptions
+
     // this._env = env  TODO
-    this._funOptions = funOptions
+    this._handlerName = handlerName
+    this._handlerPath = handlerPath
+    this._runtime = runtime
     this._stage = stage
+
+    this._invokeLocal = this._getLocalInvoke()
+  }
+
+  _getLocalInvoke() {
+    if (['python2.7', 'python3.6', 'python3.7'].includes(this._runtime)) {
+      return this._invokeLocalPython()
+    }
+
+    if (this._runtime === 'ruby2.5') {
+      return this._invokeLocalRuby()
+    }
+
+    // TODO FIXME
+    throw new Error('Unsupported runtime')
   }
 
   // no-op
@@ -22,66 +41,128 @@ module.exports = class ServerlessInvokeLocalRunner {
   // https://github.com/serverless/serverless/blob/v1.50.0/lib/plugins/aws/invokeLocal/index.js#L410
   // invoke.py, copy/pasted entirely as is:
   // https://github.com/serverless/serverless/blob/v1.50.0/lib/plugins/aws/invokeLocal/invoke.py
-  async _invokeLocalPython(event, context) {
-    const { handlerName, handlerPath, runtime } = this._funOptions
+  _invokeLocalPython() {
+    const runtime = platform() === 'win32' ? 'python.exe' : this._runtime
 
-    const input = stringify({
-      event,
-      context,
-    })
+    return async (event, context) => {
+      const input = stringify({
+        event,
+        context,
+      })
 
-    if (process.env.VIRTUAL_ENV) {
-      const runtimeDir = platform() === 'win32' ? 'Scripts' : 'bin'
-      env.PATH = [join(env.VIRTUAL_ENV, runtimeDir), delimiter, env.PATH].join(
-        '',
+      if (process.env.VIRTUAL_ENV) {
+        const runtimeDir = platform() === 'win32' ? 'Scripts' : 'bin'
+        env.PATH = [
+          join(env.VIRTUAL_ENV, runtimeDir),
+          delimiter,
+          env.PATH,
+        ].join('')
+      }
+
+      const [pythonExecutable] = runtime.split('.')
+
+      const python = execa(
+        pythonExecutable,
+        [
+          '-u',
+          resolve(__dirname, 'invoke.py'),
+          relative(cwd(), this._handlerPath),
+          this._handlerName,
+        ],
+        {
+          env,
+          input,
+          // shell: true,
+        },
       )
+
+      let result
+
+      try {
+        result = await python
+      } catch (err) {
+        // TODO
+        console.log(err)
+
+        throw err
+      }
+
+      const { stderr: err, stdout: data } = result
+
+      if (err) {
+        // TODO
+        console.log(err)
+
+        return err
+      }
+
+      try {
+        return parse(data)
+      } catch (err) {
+        // TODO
+        console.log('No JSON')
+      }
     }
+  }
 
-    const [pythonExecutable] = runtime.split('.')
+  // invokeLocalRuby, loosely based on:
+  // https://github.com/serverless/serverless/blob/v1.50.0/lib/plugins/aws/invokeLocal/index.js#L556
+  // invoke.rb, copy/pasted entirely as is:
+  // https://github.com/serverless/serverless/blob/v1.50.0/lib/plugins/aws/invokeLocal/invoke.rb
+  _invokeLocalRuby() {
+    const runtime = platform() === 'win32' ? 'ruby.exe' : 'ruby'
 
-    const python = execa(
-      pythonExecutable,
-      [
-        '-u',
-        resolve(__dirname, 'invoke.py'),
-        relative(cwd(), handlerPath),
-        handlerName,
-      ],
-      {
-        env,
-        input,
-      },
-    )
+    // TODO FIXME
+    return async (/* event, context */) => {
+      const input = stringify({
+        event: {}, // TODO FIXME
+        context: {}, // TODO FIXME
+      })
 
-    let result
+      const ruby = execa(
+        runtime,
+        [
+          resolve(__dirname, 'invoke.rb'),
+          relative(cwd(), this._handlerPath),
+          this._handlerName,
+        ],
+        {
+          env,
+          input,
+          // shell: true,
+        },
+      )
 
-    try {
-      result = await python
-    } catch (err) {
-      // TODO
-      console.log(err)
+      let result
 
-      throw err
-    }
+      try {
+        result = await ruby
+      } catch (err) {
+        // TODO
+        console.log(err)
 
-    const { stderr: err, stdout: data } = result
+        throw err
+      }
 
-    if (err) {
-      // TODO
-      console.log(err)
+      const { stderr: err, stdout: data } = result
 
-      return err
-    }
+      if (err) {
+        // TODO
+        console.log(err)
 
-    try {
-      return parse(data)
-    } catch (err) {
-      // TODO
-      console.log('No JSON')
+        return err
+      }
+
+      try {
+        return parse(data)
+      } catch (err) {
+        // TODO
+        console.log('No JSON')
+      }
     }
   }
 
   run(event, context) {
-    return this._invokeLocalPython(event, context)
+    return this._invokeLocal(event, context)
   }
 }
