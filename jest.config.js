eb92c456dfc6e15c52a69d63e4909922c1bb1879
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-24 15:13:26 -0400

    Fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -14,9 +14,9 @@ module.exports = {
     '__tests__/integration/handler/handlerPayload.js',
     '__tests__/integration/uncategorized/uncategorized.js',
     '__tests__/manual/',
-    '__tests__/old-unit/fixtures/handler.js',
+    '__tests__/old-unit/fixtures/',
     '__tests__/old-unit/support/',
-    'src/__tests__/fixtures/lambdaFunction.fixture.js',
+    'src/__tests__/fixtures/',
   ],
   setupFiles: ['./src/polyfills.js'],
 }
