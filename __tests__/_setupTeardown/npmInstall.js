08431a8bd571434bd4224f4d110bfe0cd747993f
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-28 08:53:34 -0400

    Use function statement

--- __tests__/_setupTeardown/npmInstall.js
+++ __tests__/_setupTeardown/npmInstall.js
@@ -3,8 +3,7 @@
 const { resolve } = require('path')
 const execa = require('execa')
 
-// setup.js
-module.exports = async () => {
+module.exports = async function npmInstall() {
   return execa('npm', ['ci'], {
     cwd: resolve(__dirname, '../scenario/apollo-server-lambda'),
     stdio: 'inherit',
