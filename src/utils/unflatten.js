2f7bdf2ed1de153cae00b6a467a7be262abaf459
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-08-31 07:32:52 -0400

    Re-use length

--- src/utils/unflatten.js
+++ src/utils/unflatten.js
@@ -12,7 +12,7 @@ export default function unflatten(value, size) {
 
   const unflattened = []
 
-  for (let i = 0; i < value.length; i += size) {
+  for (let i = 0; i < length; i += size) {
     const slice = value.slice(i, i + size)
     unflattened.push(slice)
   }
