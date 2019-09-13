c2fe7a374c5d0b9a755133f94b17f53256ecda18
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-09-13 11:11:20 -0400

    Add dev bridge with @babel/register

---
+++ src/main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const { resolve } = require('path')
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@babel/register')({
+  configFile: resolve(__dirname, '../babel.config.js'),
+  ignore: [/node_modules/],
+})
+
+module.exports = require('./index.js').default
