c2cfbbfc923ce74e2c49ce3a91649d5cf98c4dd8
@computerpunc <computerpunc@gmail.com>
2019-09-22 19:39:35 +0300

    Add 1 skipped test (that is running without errors on AWS)

--- __tests__/integration/websocket/main-test.js
+++ __tests__/integration/websocket/main-test.js
@@ -9,11 +9,11 @@ import { joinUrl, setup, teardown } from '../_testHelpers/index.js'
 
 // const aws4 = require('aws4')
 // const awscred = require('awscred')
-// const moment = require('moment')
+const moment = require('moment')
 
 const endpoint = process.env.npm_config_endpoint || 'ws://localhost:3001'
 const loadOfflineServer = !process.env.npm_config_endpoint
-const timeout = process.env.npm_config_timeout ? parseInt(process.env.npm_config_timeout) : 1000
+const timeout = 30000 // process.env.npm_config_timeout ? parseInt(process.env.npm_config_timeout) : 1000
 const WebSocketTester = require('../_testHelpers/WebSocketTester')
 
 jest.setTimeout(30000)
@@ -274,6 +274,170 @@ describe('handler payload tests', () => {
     expect(disconnect3).toEqual({ action:'update', event:'disconnect', info:{ id:c3.id } })
   })
 
+  const createExpectedEvent = (connectionId, action, eventType, actualEvent) => {
+    const url = new URL(endpoint)
+    const expected = {
+      isBase64Encoded: false,
+      requestContext: {
+        apiId: actualEvent.requestContext.apiId,
+        connectedAt: actualEvent.requestContext.connectedAt,
+        connectionId: `${connectionId}`,
+        domainName: url.hostname,
+        eventType,
+        extendedRequestId: actualEvent.requestContext.extendedRequestId,
+        identity: {
+          accessKey: null,
+          accountId: null,
+          caller: null,
+          cognitoAuthenticationProvider: null,
+          cognitoAuthenticationType: null,
+          cognitoIdentityId: null,
+          cognitoIdentityPoolId: null,
+          principalOrgId: null,
+          sourceIp: actualEvent.requestContext.identity.sourceIp,
+          user: null,
+          userAgent: null,
+          userArn: null,
+        },
+        messageDirection: 'IN',
+        messageId: actualEvent.requestContext.messageId,
+        requestId: actualEvent.requestContext.requestId,
+        requestTime: actualEvent.requestContext.requestTime,
+        requestTimeEpoch: actualEvent.requestContext.requestTimeEpoch,
+        routeKey: action,
+        stage: actualEvent.requestContext.stage,
+      },
+    }
+
+    return expected;
+  }
+
+  const createExpectedContext = actualContext => {
+    const expected = {
+      awsRequestId: actualContext.awsRequestId,
+      callbackWaitsForEmptyEventLoop: true,
+      functionName: actualContext.functionName,
+      functionVersion: '$LATEST',
+      invokedFunctionArn: actualContext.invokedFunctionArn,
+      invokeid: actualContext.invokeid,
+      logGroupName: actualContext.logGroupName,
+      logStreamName: actualContext.logStreamName,
+      memoryLimitInMB: actualContext.memoryLimitInMB,
+    }
+
+    return expected
+  }
+
+  const createExpectedConnectHeaders = actualHeaders => {
+    const url = new URL(endpoint);
+    const expected = {
+      Host: `${url.hostname}${url.port ? `:${url.port}` : ''}`,
+      'Sec-WebSocket-Extensions': actualHeaders['Sec-WebSocket-Extensions'],
+      'Sec-WebSocket-Key': actualHeaders['Sec-WebSocket-Key'],
+      'Sec-WebSocket-Version': actualHeaders['Sec-WebSocket-Version'],
+      'X-Amzn-Trace-Id': actualHeaders['X-Amzn-Trace-Id'],
+      'X-Forwarded-For': actualHeaders['X-Forwarded-For'],
+      'X-Forwarded-Port': `${url.port || 443}`,
+      'X-Forwarded-Proto': `${url.protocol.replace('ws', 'http').replace('wss', 'https').replace(':', '')}`,
+    }
+
+    return expected;
+  }
+
+  const createExpectedDisconnectHeaders = () => {
+    const url = new URL(endpoint);
+    const expected = {
+      Host: url.hostname,
+      'X-Forwarded-For': '',
+      'x-api-key': '',
+      'x-restapi': '',
+    };
+
+    return expected
+  }
+
+  const createExpectedConnectMultiValueHeaders = actualHeaders => {
+    const expected = createExpectedConnectHeaders(actualHeaders)
+    Object.keys(expected).forEach(key => {
+      expected[key] = [expected[key]]
+    });
+
+    return expected
+  }
+
+  const createExpectedDisconnectMultiValueHeaders = actualHeaders => {
+    const expected = createExpectedDisconnectHeaders(actualHeaders)
+    Object.keys(expected).forEach(key => {
+      expected[key] = [expected[key]]
+    })
+
+    return expected
+  }
+
+  expect.extend({
+    toBeWithinRange(received, floor, ceiling) {
+      const pass = received >= floor && received <= ceiling;
+      if (pass) {
+        return {
+          message: () =>
+            `expected ${received} not to be within range ${floor} - ${ceiling}`,
+          pass: true,
+        };
+      } else {
+        return {
+          message: () =>
+            `expected ${received} to be within range ${floor} - ${ceiling}`,
+          pass: false,
+        };
+      }
+    },
+  })
+
+  test.skip('should receive correct call info (event only)', async () => {
+    const ws = await createWebSocket()
+    await ws.send(JSON.stringify({ action:'registerListener' }))
+    await ws.receive1()
+
+    // connect
+    const c = await createClient()
+    const connect = JSON.parse(await ws.receive1())
+    let now = Date.now()
+    let expectedCallInfo = { id:c.id, event:{ headers:createExpectedConnectHeaders(connect.info.event.headers), multiValueHeaders:createExpectedConnectMultiValueHeaders(connect.info.event.headers), ...createExpectedEvent(c.id, '$connect', 'CONNECT', connect.info.event) }, context:createExpectedContext(connect.info.context) }
+    delete connect.info.context; delete expectedCallInfo.context // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
+    delete connect.info.event.headers['user-agent']; delete connect.info.event.multiValueHeaders['user-agent']
+
+    expect(connect).toEqual({ action:'update', event:'connect', info:expectedCallInfo })
+    expect(connect.info.event.requestContext.requestTimeEpoch).toBeWithinRange(connect.info.event.requestContext.connectedAt - 10, connect.info.event.requestContext.requestTimeEpoch + 10)
+    expect(connect.info.event.requestContext.connectedAt).toBeWithinRange(now - timeout, now)
+    expect(connect.info.event.requestContext.requestTimeEpoch).toBeWithinRange(now - timeout, now)
+    expect(moment.utc(connect.info.event.requestContext.requestTime, 'D/MMM/YYYY:H:m:s Z').toDate().getTime()).toBeWithinRange(now - timeout, now)
+
+    if (endpoint.startsWith('ws://locahost')) {
+      expect(connect.info.event.headers['X-Forwarded-For']).toEqual('127.0.0.1')
+    }
+
+    // getCallInfo
+    c.ws.send(JSON.stringify({ action:'getCallInfo' }))
+    const callInfo = JSON.parse(await c.ws.receive1())
+    now = Date.now()
+    expectedCallInfo = { event:{ body: '{"action":"getCallInfo"}', ...createExpectedEvent(c.id, 'getCallInfo', 'MESSAGE', callInfo.info.event) }, context:createExpectedContext(callInfo.info.context) }
+    delete callInfo.info.context; delete expectedCallInfo.context // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
+
+    expect(callInfo).toEqual({ action:'update', event:'call-info', info:expectedCallInfo })
+    expect(callInfo.info.event.requestContext.connectedAt).toBeLessThan(callInfo.info.event.requestContext.requestTimeEpoch)
+    expect(callInfo.info.event.requestContext.connectedAt).toBeWithinRange(now - timeout, now)
+    expect(callInfo.info.event.requestContext.requestTimeEpoch).toBeWithinRange(now - timeout, now)
+    expect(moment.utc(callInfo.info.event.requestContext.requestTime, 'D/MMM/YYYY:H:m:s Z').toDate().getTime()).toBeWithinRange(now - timeout, now)
+
+    // disconnect
+    c.ws.close()
+    const disconnect = JSON.parse(await ws.receive1())
+    now = Date.now()
+    expectedCallInfo = { id:c.id, event:{ headers:createExpectedDisconnectHeaders(disconnect.info.event.headers), multiValueHeaders:createExpectedDisconnectMultiValueHeaders(disconnect.info.event.headers), ...createExpectedEvent(c.id, '$disconnect', 'DISCONNECT', disconnect.info.event) }, context:createExpectedContext(disconnect.info.context) }
+    delete disconnect.info.context; delete expectedCallInfo.context // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
+    expect(disconnect).toEqual({ action:'update', event:'disconnect', info:expectedCallInfo })
+  })
+
 })
 
 
@@ -365,150 +529,7 @@ describe('handler payload tests', () => {
 
 
 
-//     const createExpectedEvent = (connectionId, action, eventType, actualEvent) => {
-//       const url = new URL(endpoint);
-//       const expected = {
-//         isBase64Encoded: false,
-//         requestContext: {
-//           apiId: actualEvent.requestContext.apiId,
-//           connectedAt: actualEvent.requestContext.connectedAt,
-//           connectionId: `${connectionId}`,
-//           domainName: url.hostname,
-//           eventType,
-//           extendedRequestId: actualEvent.requestContext.extendedRequestId,
-//           identity: {
-//             accessKey: null,
-//             accountId: null,
-//             caller: null,
-//             cognitoAuthenticationProvider: null,
-//             cognitoAuthenticationType: null,
-//             cognitoIdentityId: null,
-//             cognitoIdentityPoolId: null,
-//             principalOrgId: null,
-//             sourceIp: actualEvent.requestContext.identity.sourceIp,
-//             user: null,
-//             userAgent: null,
-//             userArn: null,
-//           },
-//           messageDirection: 'IN',
-//           messageId: actualEvent.requestContext.messageId,
-//           requestId: actualEvent.requestContext.requestId,
-//           requestTime: actualEvent.requestContext.requestTime,
-//           requestTimeEpoch: actualEvent.requestContext.requestTimeEpoch,
-//           routeKey: action,
-//           stage: actualEvent.requestContext.stage,
-//         },
-//       };
 
-//       return expected;
-//     };
-
-//     const createExpectedContext = actualContext => {
-//       const expected = {
-//         awsRequestId: actualContext.awsRequestId,
-//         callbackWaitsForEmptyEventLoop: true,
-//         functionName: actualContext.functionName,
-//         functionVersion: '$LATEST',
-//         invokedFunctionArn: actualContext.invokedFunctionArn,
-//         invokeid: actualContext.invokeid,
-//         logGroupName: actualContext.logGroupName,
-//         logStreamName: actualContext.logStreamName,
-//         memoryLimitInMB: actualContext.memoryLimitInMB,
-//       };
-
-//       return expected;
-//     };
-
-//     const createExpectedConnectHeaders = actualHeaders => {
-//       const url = new URL(endpoint);
-//       const expected = {
-//         Host: `${url.hostname}${url.port ? `:${url.port}` : ''}`,
-//         'Sec-WebSocket-Extensions': actualHeaders['Sec-WebSocket-Extensions'],
-//         'Sec-WebSocket-Key': actualHeaders['Sec-WebSocket-Key'],
-//         'Sec-WebSocket-Version': actualHeaders['Sec-WebSocket-Version'],
-//         'X-Amzn-Trace-Id': actualHeaders['X-Amzn-Trace-Id'],
-//         'X-Forwarded-For': actualHeaders['X-Forwarded-For'],
-//         'X-Forwarded-Port': `${url.port || 443}`,
-//         'X-Forwarded-Proto': `${url.protocol.replace('ws', 'http').replace('wss', 'https').replace(':', '')}`,
-//       };
-
-//       return expected;
-//     };
-
-//     const createExpectedDisconnectHeaders = () => {
-//       const url = new URL(endpoint);
-//       const expected = {
-//         Host: url.hostname,
-//         'X-Forwarded-For': '',
-//         'x-api-key': '',
-//         'x-restapi': '',
-//       };
-
-//       return expected;
-//     };
-
-//     const createExpectedConnectMultiValueHeaders = actualHeaders => {
-//       const expected = createExpectedConnectHeaders(actualHeaders);
-//       Object.keys(expected).forEach(key => {
-//         expected[key] = [expected[key]];
-//       });
-
-//       return expected;
-//     };
-
-//     const createExpectedDisconnectMultiValueHeaders = actualHeaders => {
-//       const expected = createExpectedDisconnectHeaders(actualHeaders);
-//       Object.keys(expected).forEach(key => {
-//         expected[key] = [expected[key]];
-//       });
-
-//       return expected;
-//     };
-
-//     it('should receive correct call info (event only)', async () => {
-//       const ws = await createWebSocket();
-//       await ws.send(JSON.stringify({ action:'registerListener' }));
-//       await ws.receive1();
-
-//       // connect
-//       const c = await createClient();
-//       const connect = JSON.parse(await ws.receive1());
-//       let now = Date.now();
-//       let expectedCallInfo = { id:c.id, event:{ headers:createExpectedConnectHeaders(connect.info.event.headers), multiValueHeaders:createExpectedConnectMultiValueHeaders(connect.info.event.headers), ...createExpectedEvent(c.id, '$connect', 'CONNECT', connect.info.event) }, context:createExpectedContext(connect.info.context) };
-//       delete connect.info.context; delete expectedCallInfo.context; // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
-//       delete connect.info.event.headers['user-agent']; delete connect.info.event.multiValueHeaders['user-agent'];
-
-//       expect(connect).to.deep.equal({ action:'update', event:'connect', info:expectedCallInfo });
-//       expect(connect.info.event.requestContext.requestTimeEpoch).to.be.within(connect.info.event.requestContext.connectedAt - 10, connect.info.event.requestContext.requestTimeEpoch + 10);
-//       expect(connect.info.event.requestContext.connectedAt).to.be.within(now - timeout, now);
-//       expect(connect.info.event.requestContext.requestTimeEpoch).to.be.within(now - timeout, now);
-//       expect(moment.utc(connect.info.event.requestContext.requestTime, 'D/MMM/YYYY:H:m:s Z').toDate().getTime()).to.be.within(now - timeout, now);
-
-//       if (endpoint.startsWith('ws://locahost')) {
-//         expect(connect.info.event.headers['X-Forwarded-For']).to.be.equal('127.0.0.1');
-//       }
-
-//       // getCallInfo
-//       c.ws.send(JSON.stringify({ action:'getCallInfo' }));
-//       const callInfo = JSON.parse(await c.ws.receive1());
-//       now = Date.now();
-//       expectedCallInfo = { event:{ body: '{"action":"getCallInfo"}', ...createExpectedEvent(c.id, 'getCallInfo', 'MESSAGE', callInfo.info.event) }, context:createExpectedContext(callInfo.info.context) };
-//       delete callInfo.info.context; delete expectedCallInfo.context; // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
-
-//       expect(callInfo).to.deep.equal({ action:'update', event:'call-info', info:expectedCallInfo });
-//       expect(callInfo.info.event.requestContext.connectedAt).to.be.lt(callInfo.info.event.requestContext.requestTimeEpoch);
-//       expect(callInfo.info.event.requestContext.connectedAt).to.be.within(now - timeout, now);
-//       expect(callInfo.info.event.requestContext.requestTimeEpoch).to.be.within(now - timeout, now);
-//       expect(moment.utc(callInfo.info.event.requestContext.requestTime, 'D/MMM/YYYY:H:m:s Z').toDate().getTime()).to.be.within(now - timeout, now);
-
-//       // disconnect
-//       c.ws.close();
-//       const disconnect = JSON.parse(await ws.receive1());
-//       now = Date.now();
-//       expectedCallInfo = { id:c.id, event:{ headers:createExpectedDisconnectHeaders(disconnect.info.event.headers), multiValueHeaders:createExpectedDisconnectMultiValueHeaders(disconnect.info.event.headers), ...createExpectedEvent(c.id, '$disconnect', 'DISCONNECT', disconnect.info.event) }, context:createExpectedContext(disconnect.info.context) };
-//       delete disconnect.info.context; delete expectedCallInfo.context; // Not checking context. Relying on it to be correct because serverless-offline uses general lambda context method
-//       expect(disconnect).to.deep.equal({ action:'update', event:'disconnect', info:expectedCallInfo });
-//     }).timeout(timeout);
 
 //     it('should be able to parse query string in connect', async () => {
 //       const now = `${Date.now()}`;
