9f671157c52840320c8948660c8a0313057a4368
@computerpunc <computerpunc@gmail.com>
2019-09-24 10:13:11 +0300

    Fixed lint

--- __tests__/integration/websocket/RouteSelection/RouteSelection-handler.js
+++ __tests__/integration/websocket/RouteSelection/RouteSelection-handler.js
@@ -1,29 +1,33 @@
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 
 const successfullResponse = {
   statusCode: 200,
   body: 'Request is OK.',
-};
+}
 
-module.exports.echo = async (event, context) => {
-  const action = JSON.parse(event.body);
+const newAWSApiGatewayManagementApi = (event) => {
+  const endpoint = process.env.IS_OFFLINE
+    ? 'http://localhost:3005'
+    : `${event.requestContext.domainName}/${event.requestContext.stage}`
+  const apiVersion = '2018-11-29'
 
-  await sendToClient(action.message, event.requestContext.connectionId, newAWSApiGatewayManagementApi(event, context));
-
-  return successfullResponse;
-};
-
-const newAWSApiGatewayManagementApi = event => {
-  const endpoint = process.env.IS_OFFLINE ? 'http://localhost:3005' : `${event.requestContext.domainName}/${event.requestContext.stage}`;
-  const apiVersion = '2018-11-29';
-
-  return new AWS.ApiGatewayManagementApi({ apiVersion, endpoint });
-};
+  return new AWS.ApiGatewayManagementApi({ apiVersion, endpoint })
+}
 
 const sendToClient = (data, connectionId, apigwManagementApi) => {
-  // console.log(`sendToClient:${connectionId}`);
-  let sendee = data;
-  if (typeof data === 'object') sendee = JSON.stringify(data);
+  // console.log(`sendToClient:${connectionId}`)
+  let sendee = data
+  if (typeof data === 'object') sendee = JSON.stringify(data)
 
-  return apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: sendee }).promise();
-};
+  return apigwManagementApi
+    .postToConnection({ ConnectionId: connectionId, Data: sendee })
+    .promise()
+}
+
+module.exports.echo = async (event, context) => {
+  const action = JSON.parse(event.body)
+
+  await sendToClient(action.message, event.requestContext.connectionId, newAWSApiGatewayManagementApi(event, context)) // eslint-disable-line 
+
+  return successfullResponse
+}
--- __tests__/integration/websocket/RouteSelection/RouteSelection-test.js
+++ __tests__/integration/websocket/RouteSelection/RouteSelection-test.js
@@ -2,52 +2,41 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-expressions */
 import { resolve } from 'path'
-import fetch from 'node-fetch'
-import { joinUrl, setup, teardown } from '../../_testHelpers/index.js'
-
-const aws4 = require('aws4')
-const awscred = require('awscred')
-const moment = require('moment')
+import { setup, teardown } from '../../_testHelpers/index.js'
 
 const endpoint = process.env.npm_config_endpoint || 'ws://localhost:3005'
 const loadOfflineServer = !process.env.npm_config_endpoint
-const timeout = 30000 // process.env.npm_config_timeout ? parseInt(process.env.npm_config_timeout) : 1000
+// const timeout = 30000 // process.env.npm_config_timeout ? parseInt(process.env.npm_config_timeout) : 1000
 const WebSocketTester = require('../../_testHelpers/WebSocketTester')
 
 jest.setTimeout(30000)
 
 describe('handler payload tests', () => {
+  let clients = []
 
-  let clients = []; let req = null; let cred = null;
-  const createWebSocket = async options => {
-    const ws = new WebSocketTester();
-    let url = endpoint; let wsOptions = null;
-    if (options && options.url) url = options.url; // eslint-disable-line prefer-destructuring
-    if (options && options.qs) url = `${url}?${options.qs}`;
-    if (options && options.headers) wsOptions = { headers:options.headers };
-    const hasOpened = await ws.open(url, wsOptions);
+  const createWebSocket = async (options) => {
+    const ws = new WebSocketTester()
+    let url = endpoint
+    let wsOptions = null
+    if (options && options.url) url = options.url // eslint-disable-line prefer-destructuring
+    if (options && options.qs) url = `${url}?${options.qs}`
+    if (options && options.headers) wsOptions = { headers: options.headers }
+    const hasOpened = await ws.open(url, wsOptions)
     if (!hasOpened) {
-      try { ws.close(); } catch (err) {} // eslint-disable-line brace-style, no-empty
-      
-      return;
+      try {
+        ws.close()
+      } catch (err) {} // eslint-disable-line brace-style, no-empty
+
+      return null
     }
-    clients.push(ws);
+    clients.push(ws)
 
-    return ws;
-  };
-  const createClient = async options => {
-    const ws = await createWebSocket(options);
+    return ws
+  }
 
-    ws.send(JSON.stringify({ action:'getClientInfo' }));
-
-    const json = await ws.receive1();
-    const {id} = JSON.parse(json).info;
-
-    return { ws, id };
-  };
   // init
   beforeAll(async () => {
-    if (!loadOfflineServer) return
+    if (!loadOfflineServer) return null
     return setup({
       servicePath: resolve(__dirname),
     })
@@ -55,41 +44,41 @@ describe('handler payload tests', () => {
 
   // cleanup
   afterAll(() => {
-    if (!loadOfflineServer) return
+    if (!loadOfflineServer) return null
     return teardown()
   })
 
   beforeEach(() => {
     clients = []
-  });
+  })
+
   afterEach(async () => {
-    const unreceived0 = clients.map(c=>0)
-    const unreceived = clients.map(c=>0)
-    await Promise.all(clients.map(async (ws, i) => {
-      const n = ws.countUnrecived()
-      unreceived[i] = n
+    // const unreceived0 = clients.map(c=>0)
+    const unreceived = clients.map(() => 0)
+    await Promise.all(
+      clients.map(async (ws, i) => {
+        const n = ws.countUnrecived()
+        unreceived[i] = n
 
-      if (n > 0) {
-        console.log(`unreceived:[i=${i}]`);
-        (await ws.receive(n)).forEach(m => console.log(m))
-      }
+        if (n > 0) {
+          console.log(`unreceived:[i=${i}]`)
+          ;(await ws.receive(n)).forEach((m) => console.log(m))
+        }
 
-      ws.close()
-    }));
+        ws.close()
+      }),
+    )
     // expect(unreceived).to.be.deep.equal(unreceived0)
     clients = []
-  });
+  })
 
-  const httpUrl = `${endpoint.replace('ws://', 'http://').replace('wss://', 'https://')}`
-
-  test('should call action \'echo\' handler located at service.do', async () => {
+  test('should call action "echo" handler located at service.do', async () => {
     const ws = await createWebSocket()
     const now = `${Date.now()}`
-    const payload = JSON.stringify({ service:{ do:'echo' }, message:now })
+    const payload = JSON.stringify({ service: { do: 'echo' }, message: now })
 
     ws.send(payload)
 
     expect(await ws.receive1()).toEqual(`${now}`)
   })
-
 })
