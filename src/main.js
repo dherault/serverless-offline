f6f3cd56bc210cae62b676faa274f64bf40b1b2c
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-10-17 12:46:12 -0400

    Fix babel es-module build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 'use strict'
 
-const { resolve } = require('path')
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-require('@babel/register')({
-  configFile: resolve(__dirname, '../babel.config.js'),
-  ignore: [/node_modules/],
-})
+// UNCOMMENT FOR DEVELOPMENT:
+//
+// const { resolve } = require('path')
+//
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// require('@babel/register')({
+//   configFile: resolve(__dirname, '../babel.config.js'),
+//   ignore: [/node_modules/],
+// })
 
 module.exports = require('./index.js').default
