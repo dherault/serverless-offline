d8c8ad296b2ea8e0fd173fc8ed7a5ca8d472ad6d
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-10-19 08:33:25 -0400

    Fix unit tests

--- __tests__/old-unit/support/ServerlessBuilder.js
+++ __tests__/old-unit/support/ServerlessBuilder.js
@@ -10,8 +10,22 @@ export default class ServerlessBuilder {
       },
       service: {
         functions: {},
-        getFunction(functionKey) {
-          return this.functions[functionKey]
+        // https://github.com/serverless/serverless/blob/v1.54.0/lib/classes/Service.js#L250
+        getAllEventsInFunction(functionName) {
+          return this.getFunction(functionName).events
+        },
+        // https://github.com/serverless/serverless/blob/v1.54.0/lib/classes/Service.js#L216
+        getAllFunctions() {
+          return Object.keys(this.functions)
+        },
+        // https://github.com/serverless/serverless/blob/v1.54.0/lib/classes/Service.js#L228
+        getFunction(functionName) {
+          if (functionName in this.functions) {
+            return this.functions[functionName]
+          }
+          throw new Error(
+            `Function "${functionName}" doesn't exist in this Service`,
+          )
         },
         provider: {
           name: 'aws',
