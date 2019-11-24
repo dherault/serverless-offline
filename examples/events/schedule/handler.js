28bbf0c87f6cd4af0f0ff9f6fcb40acff2850030
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-11-24 14:49:59 -0500

    Add additional schedule example

--- examples/events/schedule/handler.js
+++ examples/events/schedule/handler.js
@@ -1,12 +1,9 @@
 'use strict'
 
-// const { stringify } = JSON
+exports.schedule1 = async function schedule1() {
+  console.log('Scheduler 1 running ...')
+}
 
-exports.hello = async function hello() {
-  // return {
-  //   body: stringify({ foo: 'bar' }),
-  //   statusCode: 200,
-  // }
-
-  console.log('Scheduler run ...')
+exports.schedule2 = async function schedule2(event) {
+  console.log('Scheduler 2 running ...', event)
 }
