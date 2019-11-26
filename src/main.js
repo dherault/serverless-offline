f7f5df6f694b88f416287a899cea1b61bad932a3
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-11-25 22:52:03 -0500

    Use ts-node/register for development

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
 'use strict'
 
 // UNCOMMENT FOR DEVELOPMENT:
-//
-// const { resolve } = require('path')
-//
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// require('@babel/register')({
-//   configFile: resolve(__dirname, '../babel.config.js'),
-//   ignore: [/node_modules/],
-// })
+// eslint-disable-next-line import/no-extraneous-dependencies
+// require('ts-node').register()
 
 // eslint-disable-next-line import/no-unresolved
-module.exports = require('./index.js').default
+module.exports = require('./index').default
