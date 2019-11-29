9a9d819abae655785c7b855c351da2be7a7acc03
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-11-29 11:45:57 -0500

    Remove console log

--- src/lambda/HttpServer.js
+++ src/lambda/HttpServer.js
@@ -22,8 +22,6 @@ export default class HttpServer {
     const route = invokeRoute(this._lambda)
     this._server.route(route)
 
-    console.log('hier')
-
     const { host, httpsProtocol, lambdaPort } = this._options
 
     try {
