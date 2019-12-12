6fea7fed69c0dd95554e3466ad225018e3ce09b5
Daniel Nalborczyk <dnalborczyk@gmail.com>
2019-12-12 14:00:23 -0500

    Add docker node.js 8.10 test

---
+++ tests/integration/docker/nodejs/nodejs8.10/dockerNodejs8.10.test.js
@@ -0,0 +1,42 @@
+import { resolve } from 'path'
+import fetch from 'node-fetch'
+import { satisfies } from 'semver'
+import { joinUrl, setup, teardown } from '../../../_testHelpers/index.js'
+
+jest.setTimeout(120000)
+
+describe('Node.js 8.10 with Docker tests', () => {
+  if (!process.env.DOCKER_DETECTED) {
+    it.only("Could not find 'Docker' executable, skipping 'Docker' tests.", () => {})
+  } else {
+    // init
+    beforeAll(() =>
+      setup({
+        servicePath: resolve(__dirname),
+      }),
+    )
+
+    // cleanup
+    afterAll(() => teardown())
+  }
+
+  //
+  ;[
+    {
+      description: 'should work with nodejs10.x in docker container',
+      expected: {
+        message: 'Hello Node.js 8.10!',
+      },
+      path: '/hello',
+    },
+  ].forEach(({ description, expected, path }) => {
+    test(description, async () => {
+      const url = joinUrl(TEST_BASE_URL, path)
+      const response = await fetch(url)
+      const json = await response.json()
+
+      expect(json.message).toEqual(expected.message)
+      expect(satisfies(json.version, '8.10')).toEqual(true)
+    })
+  })
+})
