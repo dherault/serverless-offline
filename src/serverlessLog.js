55968322ca659422bfa47c2cafef7eace6759d33
Daniel Nalborczyk <dnalborczyk@gmail.com>
2020-01-03 18:35:49 -0500

    Add additional cli colors for http methods

--- src/serverlessLog.js
+++ src/serverlessLog.js
@@ -6,7 +6,20 @@ const { max } = Math
 const blue = chalk.keyword('dodgerblue')
 const grey = chalk.keyword('grey')
 const lime = chalk.keyword('lime')
+const orange = chalk.keyword('orange')
+const peachpuff = chalk.keyword('peachpuff')
+const plum = chalk.keyword('plum')
 const red = chalk.keyword('red')
+const yellow = chalk.keyword('yellow')
+
+const colorMethodMapping = new Map([
+  ['DELETE', red],
+  ['GET', blue],
+  // ['HEAD', ...],
+  ['PATCH', orange],
+  ['POST', plum],
+  ['PUT', blue],
+])
 
 let log
 
@@ -24,7 +37,10 @@ export function setLog(serverlessLogRef) {
 // https://github.com/serverless/serverless/blob/master/lib/classes/CLI.js
 
 function logRoute(httpMethod, server, stage, path, maxLength) {
-  return `${blue(`${httpMethod.padEnd(maxLength, ' ')} |`)} ${grey.dim(
+  const methodColor = colorMethodMapping.get(httpMethod) || peachpuff
+  const methodFormatted = httpMethod.padEnd(maxLength, ' ')
+
+  return `${methodColor(methodFormatted)} ${yellow.dim('|')} ${grey.dim(
     `${server}/${stage}`,
   )}${lime(path)}`
 }
@@ -34,13 +50,13 @@ function getMaxHttpMethodNameLength(routeInfo) {
 }
 
 export function logRoutes(routeInfo) {
-  const maxLength = getMaxHttpMethodNameLength(routeInfo)
   const boxenOptions = {
     borderColor: 'yellow',
     dimBorder: true,
     margin: 1,
     padding: 1,
   }
+  const maxLength = getMaxHttpMethodNameLength(routeInfo)
 
   console.log(
     boxen(
