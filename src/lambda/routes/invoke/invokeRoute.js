72a0e4ba1072c00b7c546891181e78ddad7a4de6
Daniel Nalborczyk <dnalborczyk@gmail.com>
2020-01-03 21:19:17 -0500

    Order nit

--- src/lambda/routes/invoke/invokeRoute.js
+++ src/lambda/routes/invoke/invokeRoute.js
@@ -9,17 +9,6 @@ export default function invokeRoute(lambda) {
   const invokeController = new InvokeController(lambda)
 
   return {
-    method: 'POST',
-    options: {
-      payload: {
-        // allow: ['binary/octet-stream'],
-        defaultContentType: 'binary/octet-stream',
-        // request.payload will be a raw buffer
-        parse: false,
-      },
-      tags: ['api'],
-    },
-    path: '/{apiVersion}/functions/{functionName}/invocations',
     handler(request) {
       const {
         headers,
@@ -51,5 +40,16 @@ export default function invokeRoute(lambda) {
         clientContext,
       )
     },
+    method: 'POST',
+    options: {
+      payload: {
+        // allow: ['binary/octet-stream'],
+        defaultContentType: 'binary/octet-stream',
+        // request.payload will be a raw buffer
+        parse: false,
+      },
+      tags: ['api'],
+    },
+    path: '/{apiVersion}/functions/{functionName}/invocations',
   }
 }
