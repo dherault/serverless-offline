70c9bc7b9aaa79ce4926bff87895f25f039e9704
Alex Vance <avance@dotsln.com>
2020-05-11 02:13:29 -0500

    the function is being invoked now

---
+++ tests/integration/dotnetcore/dotnetcore3.1/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
+if [ -f /etc/debian_version ]
+then
+  apt -qq update
+  apt -qq -y install zip
+fi
+
+dotnet restore
+dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/release/netcoreapp3.1/hello.zip
--- tests/integration/dotnetcore/dotnetcore3.1/handler.cs
+++ tests/integration/dotnetcore/dotnetcore3.1/handler.cs
@@ -2,26 +2,26 @@ using System.IO;
 
 namespace Example
 {
-  public class LambdaRequest
-  {
-    public string body { get; set; }
-  }
-
-  public class LambdaResponse
-  {
-    public int statusCode { get; set; }
-    public string body { get; set; }
-  }
-
-  public class Hello
-  {
-    public LambdaResponse MyHandler(LambdaRequest request)
+    public class LambdaRequest
     {
-       return new LambdaResponse
-       {
-         statusCode = 200,
-         body = "{\"message\": \"Hello from .NET Core 3.1!\"}"
-       };
+        public string body { get; set; }
     }
-  }
+
+    public class LambdaResponse
+    {
+        public int statusCode { get; set; }
+        public string body { get; set; }
+    }
+
+    public class Hello
+    {
+        public LambdaResponse MyHandler(LambdaRequest request)
+        {
+            return new LambdaResponse
+            {
+                statusCode = 200,
+                body = "{\"message\": \"Hello from .NET Core 3.1!\"}"
+            };
+        }
+    }
 }
