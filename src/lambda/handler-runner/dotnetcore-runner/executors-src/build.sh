bde90130de445d9fd9be91c89a8a240cce66fdd5
Alex Vance <avance@dotsln.com>
2020-05-29 15:45:22 -0500

    fix build.sh

--- src/lambda/handler-runner/dotnetcore-runner/executors-src/build.sh
+++ src/lambda/handler-runner/dotnetcore-runner/executors-src/build.sh
@@ -3,5 +3,5 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-dotnet build && dotnet publish dotnetcore3.1/dotnetcore3.1.csproj -c Release -o ../executors-binaries/dotnetcore3.1 --verbosity detailed
+dotnet build dotnetcore3.1/dotnetcore3.1.csproj && dotnet publish dotnetcore3.1/dotnetcore3.1.csproj -c Release -o ../executors-binaries/dotnetcore3.1 --verbosity detailed
 #dotnet publish dotnetcore2.1/dotnetcore2.1.csproj -c Release -o ../executors-binaries/dotnetcore2.1
