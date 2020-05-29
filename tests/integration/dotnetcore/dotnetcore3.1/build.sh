ce9909b5f96cce2937788acb43140807cce53323
Alex Vance <avance@dotsln.com>
2020-05-29 15:18:28 -0500

    trying to remove something

--- tests/integration/dotnetcore/dotnetcore3.1/build.sh
+++ tests/integration/dotnetcore/dotnetcore3.1/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
-if [ -f /etc/debian_version ]
-then
-  apt -qq update
-  apt -qq -y install zip
-fi
+# #install zip on debian OS, since microsoft/dotnet container doesn't have zip by default
+# if [ -f /etc/debian_version ]
+# then
+#   apt -qq update
+#   apt -qq -y install zip
+# fi
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
