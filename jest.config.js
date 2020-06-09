22b075132d08dc530a25e05577fcb5a8b10f6201
Richard Tan <chardos@gmail.com>
2020-06-10 08:39:11 +1000

    refactor: Script for running tests with --watch

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,9 @@ module.exports = {
     RUN_TEST_AGAINST_AWS: AWS_ENDPOINT != null,
     TEST_BASE_URL: AWS_ENDPOINT || 'http://localhost:3000',
   },
-  globalSetup: './tests/_setupTeardown/npmInstall.js',
   modulePathIgnorePatterns: ['src/lambda/__tests__/fixtures/'],
   setupFiles: ['object.fromentries/auto.js'],
+  ...(!process.env.SKIP_SETUP && {
+    globalSetup: './tests/_setupTeardown/npmInstall.js',
+  }),
 }
