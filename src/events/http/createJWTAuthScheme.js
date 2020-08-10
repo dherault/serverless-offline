95eba5e48a5809dad4d2eb276cab091dec26e190
Lee Henson <lee.m.henson@gmail.com>
2020-08-10 22:00:56 +0100

    handle array-based jwt audience data

--- src/events/http/createJWTAuthScheme.js
+++ src/events/http/createJWTAuthScheme.js
@@ -53,10 +53,15 @@ export default function createAuthScheme(jwtOptions) {
           return Boom.unauthorized('JWT Token not from correct issuer url')
         }
 
-        if (
-          !jwtOptions.audience.includes(aud) &&
-          !jwtOptions.audience.includes(clientId)
-        ) {
+        const validAudiences = Array.isArray(jwtOptions.audience)
+          ? jwtOptions.audience
+          : [jwtOptions.audience]
+        const providedAudiences = Array.isArray(aud) ? aud : [aud]
+        const validAudienceProvided = providedAudiences.some((a) =>
+          validAudiences.includes(a),
+        )
+
+        if (!validAudienceProvided && !jwtOptions.audience.includes(clientId)) {
           serverlessLog(`JWT Token not from correct audience`)
           return Boom.unauthorized('JWT Token not from correct audience')
         }
