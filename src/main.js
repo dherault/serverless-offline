e2a6e2eff751867917923d83c0aa15c8ff1dacf8
Bryan Vaz <1505615-bryanvaz@users.noreply.gitlab.com>
2020-08-30 01:22:30 -0400

    revert main.js so it doesn't change

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 'use strict'
 
 // UNCOMMENT FOR DEVELOPMENT:
-
+//
 // const { resolve } = require('path')
-
+//
 // // eslint-disable-next-line import/no-extraneous-dependencies
 // require('@babel/register')({
 //   configFile: resolve(__dirname, '../babel.config.js'),
