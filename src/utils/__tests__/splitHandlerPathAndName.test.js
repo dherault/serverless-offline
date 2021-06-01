3178501df80bd0865234a6bb842ed57add06b882
Justin Ng <justin@aletheon.co>
2021-06-01 14:20:54 -0700

    feat: Support Ruby handlers with `::` in path definition (#1218)

---
+++ src/utils/__tests__/splitHandlerPathAndName.test.js
@@ -0,0 +1,53 @@
+import splitHandlerPathAndName from '../splitHandlerPathAndName.js'
+
+const tests = [
+  {
+    description: 'ruby handler with namespace resolution operator ::',
+    expected: ['./src/somefolder/source', 'LambdaFunctions::Handler.process'],
+    handler: './src/somefolder/source.LambdaFunctions::Handler.process',
+  },
+  {
+    description: 'ruby handler with multiple namespace resolution operators ::',
+    expected: [
+      './src/somefolder/source',
+      'Functions::LambdaFunctions::Handler.process',
+    ],
+    handler:
+      './src/somefolder/source.Functions::LambdaFunctions::Handler.process',
+  },
+  {
+    description: 'ruby handler with namespace resolution operator ::, unnested',
+    expected: ['source', 'LambdaFunctions::Handler.process'],
+    handler: 'source.LambdaFunctions::Handler.process',
+  },
+  {
+    description:
+      'ruby handler with multiple namespace resolution operators ::, unnested',
+    expected: ['./source', 'Functions::LambdaFunctions::Handler.process'],
+    handler: './source.Functions::LambdaFunctions::Handler.process',
+  },
+  {
+    description: 'ruby handler from kernel',
+    expected: ['./src/somefolder/function', 'handler'],
+    handler: './src/somefolder/function.handler',
+  },
+  {
+    description: 'generic handler',
+    expected: ['./src/somefolder/.handlers/handler', 'run'],
+    handler: './src/somefolder/.handlers/handler.run',
+  },
+  {
+    description: 'generic handler, unnested',
+    expected: ['handler', 'run'],
+    handler: 'handler.run',
+  },
+]
+
+describe('splitHandlerPathAndName', () => {
+  tests.forEach(({ description, expected, handler }) => {
+    test(`should split ${description}`, () => {
+      const result = splitHandlerPathAndName(handler)
+      expect(result).toEqual(expected)
+    })
+  })
+})
