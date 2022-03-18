6462373ac5b570a9c91b2d85b9fab473265d5f4c
Piotr Grzesik <pj.grzesik@gmail.com>
2022-03-18 10:23:01 +0100

    ci: Enforce printing offline output for debugging

--- tests/integration/artifact/local/handler1.js
+++ tests/integration/artifact/local/handler1.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const { stringify } = JSON
-
 exports.hello = async function hello() {
   return {
-    body: stringify({
+    body: JSON.stringify({
       message: 'handler1: Hello Node.js!',
     }),
     statusCode: 200,
