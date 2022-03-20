15536fc84e3d0519945ad3bffdad07100aa2eebc
Chris Sullivan <chrissullivan.dev@gmail.com>
2022-03-20 00:14:47 -0700

    Concat stderr and stdout before parsing incoming events, including latest mock-lambda

--- tests/integration/go/go1.x/hello/main.go
+++ tests/integration/go/go1.x/hello/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Println("No logs should interrupt response stdio")
+
 	return events.APIGatewayProxyResponse{
 		Body:       "{\"message\": \"Hello Go 1.x!\"}",
 		StatusCode: 200,
