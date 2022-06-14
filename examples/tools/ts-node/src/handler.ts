8a4cb01c237f9112c9d9e51c7bd8cf5b9749f39c
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-06-14 08:21:04 -0400

    chore: bump and fix prettier

--- examples/tools/ts-node/src/handler.ts
+++ examples/tools/ts-node/src/handler.ts
@@ -6,12 +6,10 @@ import {
 
 const { stringify } = JSON
 
-export const hello: Handler<
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult
-> = async function hello(event, context, callback) {
-  return {
-    body: stringify({ hello: 'ts-node' }),
-    statusCode: 200,
+export const hello: Handler<APIGatewayProxyEvent, APIGatewayProxyResult> =
+  async function hello(event, context, callback) {
+    return {
+      body: stringify({ hello: 'ts-node' }),
+      statusCode: 200,
+    }
   }
-}
