aaccf96a5905eaa60f5938b06e7b1d60b517269e
Ian Clarkson <clarksonian@gmail.com>
2022-06-14 05:45:41 -0700

    fix: don't require context (#1471) (#1472)

--- tests/integration/websocket-authorizer/websocket-authorizer.test.js
+++ tests/integration/websocket-authorizer/websocket-authorizer.test.js
@@ -8,7 +8,7 @@ import websocketSend from '../_testHelpers/websocketPromise.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
-describe.skip('websocket authorizer tests', function desc() {
+describe('websocket authorizer tests', function desc() {
   this.timeout(30000)
 
   beforeEach(() =>
@@ -45,6 +45,20 @@ describe.skip('websocket authorizer tests', function desc() {
     assert.equal(code, undefined)
     assert.equal(err.message, 'Unexpected server response: 403')
     assert.equal(data, undefined)
+  })
+
+  it('websocket authorization without context', async () => {
+    const url = new URL(joinUrl(env.TEST_BASE_URL, '/dev'))
+    url.port = url.port ? '3001' : url.port
+    url.protocol = 'ws'
+    url.searchParams.append('credential', 'noContext')
+
+    const ws = new WebSocket(url.toString())
+    const { data, code, err } = await websocketSend(ws, '{}')
+
+    assert.equal(code, undefined)
+    assert.equal(err, undefined)
+    assert.equal(data, '{}')
   })
 
   it('websocket authorization with authorizer crash', async () => {
