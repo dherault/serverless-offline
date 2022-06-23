ac63c36c29def0f899e65aedf46062948b560d06
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-06-22 22:15:39 -0400

    refactor: simplify

--- src/utils/checkDockerDaemon.js
+++ src/utils/checkDockerDaemon.js
@@ -2,13 +2,13 @@ import { execa } from 'execa'
 
 export default async function checkDockerDaemon() {
   let dockerServerOS
+
   try {
-    const { stdout } = await execa('docker', [
+    ;({ stdout: dockerServerOS } = await execa('docker', [
       'version',
       '--format',
       '{{.Server.Os}}',
-    ])
-    dockerServerOS = stdout
+    ]))
   } catch {
     throw new Error('The docker daemon is not running.')
   }
