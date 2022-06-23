390f1d931b67415c92346e6aa2471238a49934cb
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-06-23 12:18:56 -0400

    test: fix python docker tests

--- tests/mochaGlobalSetup.cjs
+++ tests/mochaGlobalSetup.cjs
@@ -14,4 +14,58 @@ exports.mochaGlobalSetup = async function mochaGlobalSetup() {
     globalThis.fetch = fetch
     globalThis.Headers = Headers
   }
+
+  const { checkDockerDaemon, checkGoVersion, detectExecutable } = await import(
+    '../src/utils/index.js'
+  )
+
+  const executables = ['java', 'python3', 'ruby']
+
+  async function detectDocker() {
+    try {
+      await checkDockerDaemon()
+    } catch {
+      return false
+    }
+    return true
+  }
+
+  // export default async function npmInstall() {
+  const [java, python3, ruby] = await Promise.all(
+    executables.map((executable) =>
+      executable === 'java'
+        ? detectExecutable('java', '-version')
+        : detectExecutable(executable),
+    ),
+  )
+
+  const docker = await detectDocker()
+
+  if (docker) {
+    env.DOCKER_DETECTED = true
+
+    const dockerCompose = await detectExecutable('docker-compose')
+
+    if (dockerCompose) {
+      env.DOCKER_COMPOSE_DETECTED = true
+    }
+  }
+
+  const go = await checkGoVersion()
+
+  if (go === '1.x') {
+    env.GO1X_DETECTED = true
+  }
+
+  if (java) {
+    env.JAVA_DETECTED = true
+  }
+
+  if (python3) {
+    env.PYTHON3_DETECTED = true
+  }
+
+  if (ruby) {
+    env.RUBY_DETECTED = true
+  }
 }
