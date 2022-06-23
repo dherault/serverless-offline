6859469677b6186101aebcf6704d71cf27b2e191
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-06-23 18:29:02 -0400

    fix: add timeout

--- tests/mochaHooks.cjs
+++ tests/mochaHooks.cjs
@@ -4,6 +4,8 @@ const { env } = require('node:process')
 
 exports.mochaHooks = {
   async beforeAll() {
+    this.timeout(30000)
+
     // env.RUN_TEST_AGAINST_AWS = env.AWS_ENDPOINT != null
     env.TEST_BASE_URL = env.AWS_ENDPOINT || 'http://localhost:3000'
 
