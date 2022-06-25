92ab186231c67e3f17f261aa22802f37fb6d1dee
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-06-25 17:21:33 -0400

    test: fix python tests

--- tests/integration/python/python-big-json/python-big-json.test.js
+++ tests/integration/python/python-big-json/python-big-json.test.js
@@ -1,15 +1,12 @@
 import assert from 'node:assert'
-import { platform } from 'node:os'
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
 import { env } from 'node:process'
+import { fileURLToPath } from 'node:url'
 import { joinUrl, setup, teardown } from '../../_testHelpers/index.js'
 
-// skipping 'Python 3' tests on Windows for now.
-// Could not find 'Python 3' executable, skipping 'Python' tests.
-const _describe =
-  env.PYTHON3_DETECTED && platform() !== 'win32' ? describe : describe.skip
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-_describe('Python 3 tests', function desc() {
+describe('Python 3 tests', function desc() {
   this.timeout(60000)
 
   beforeEach(() =>
@@ -32,7 +29,12 @@ _describe('Python 3 tests', function desc() {
       path: '/dev/hello',
     },
   ].forEach(({ description, expected, path }) => {
-    it(description, async () => {
+    it(description, async function it() {
+      // Could not find 'Python 3' executable, skipping 'Python' tests.
+      if (!env.PYTHON3_DETECTED) {
+        this.skip()
+      }
+
       const url = joinUrl(env.TEST_BASE_URL, path)
       const response = await fetch(url)
       const json = await response.json()
