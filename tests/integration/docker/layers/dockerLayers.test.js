d54b2ac231cd18bac8c489c04cfacc16a34d007c
dnalborczyk <dnalborczyk@gmail.com>
2022-06-26 15:54:57 -0400

    test: fix docker layers tests (#1488)

--- tests/integration/docker/layers/dockerLayers.test.js
+++ tests/integration/docker/layers/dockerLayers.test.js
@@ -1,12 +1,12 @@
 import assert from 'node:assert'
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
 import { env } from 'node:process'
+import { fileURLToPath } from 'node:url'
 import { joinUrl, setup, teardown } from '../../_testHelpers/index.js'
 
-// "Could not find 'Docker', skipping 'Docker' tests."
-const _describe = env.DOCKER_DETECTED ? describe : describe.skip
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-_describe('Layers with Docker tests', function desc() {
+describe('Layers with Docker tests', function desc() {
   this.timeout(120000)
 
   beforeEach(() =>
@@ -27,7 +27,12 @@ _describe('Layers with Docker tests', function desc() {
       path: '/dev/hello',
     },
   ].forEach(({ description, expected, path }) => {
-    it(description, async () => {
+    it(description, async function it() {
+      // "Could not find 'Docker', skipping tests."
+      if (!env.DOCKER_DETECTED) {
+        this.skip()
+      }
+
       const url = joinUrl(env.TEST_BASE_URL, path)
       const response = await fetch(url)
       const json = await response.json()
