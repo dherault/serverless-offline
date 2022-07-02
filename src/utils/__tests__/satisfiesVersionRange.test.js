178b4843714d0b43f1c1f941e3548fdda480bccd
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-07-02 13:23:03 -0400

    test: fix satisfiesVersionRange tests

--- src/utils/__tests__/satisfiesVersionRange.test.js
+++ src/utils/__tests__/satisfiesVersionRange.test.js
@@ -1,32 +1,55 @@
-// import satisfiesVersionRange from '../satisfiesVersionRange.js'
+import assert from 'node:assert'
+import satisfiesVersionRange from '../satisfiesVersionRange.js'
 
-describe.skip('satisfiesVersionRange', () => {
-  // describe('valid parameters', () => {
-  //   test.each`
-  //     version      | range       | expected | description
-  //     ${'1.38.0'}  | ${'>=1.38'} | ${true}  | ${'same as minimum'}
-  //     ${'1.40.0'}  | ${'>=1.38'} | ${true}  | ${'greather than minimum'}
-  //     ${'1.37.11'} | ${'>=1.38'} | ${false} | ${'less than minimum'}
-  //   `(
-  //     'should return $expected when $description is passed',
-  //     ({ expected, range, version }) => {
-  //       const result = satisfiesVersionRange(version, range)
-  //       expect(result).toEqual(expected)
-  //     },
-  //   )
-  // })
-  // describe('should throw when invalid parameters are passed', () => {
-  //   test.each`
-  //     version     | range       | description
-  //     ${'a.b.c'}  | ${'>=1.40'} | ${'invalid version'}
-  //     ${'1.40.0'} | ${'a.b.c'}  | ${'invalid range'}
-  //   `(
-  //     'should throw error when $description is passed',
-  //     ({ range, version }) => {
-  //       expect(() =>
-  //         satisfiesVersionRange(version, range),
-  //       ).toThrowErrorMatchingSnapshot()
-  //     },
-  //   )
-  // })
+describe('satisfiesVersionRange', () => {
+  const tests = [
+    {
+      description: 'same as minimum',
+      expected: true,
+      range: '>=1.38',
+      version: '1.38.0',
+    },
+    {
+      description: 'greather than minimum',
+      expected: true,
+      range: '>=1.38',
+      version: '1.40.0',
+    },
+    {
+      description: 'less than minimum',
+      expected: false,
+      range: '>=1.38',
+      version: '1.37.11',
+    },
+  ]
+
+  tests.forEach(({ description, expected, range, version }) => {
+    it(description, () => {
+      const result = satisfiesVersionRange(version, range)
+      assert.strictEqual(result, expected)
+    })
+  })
+})
+
+describe('should throw when invalid parameters are passed', () => {
+  const tests = [
+    {
+      description: 'invalid version',
+      message: 'Not a valid semver range: a.b.c',
+      range: '>=1.40',
+      version: 'a.b.c',
+    },
+    {
+      description: 'invalid range',
+      message: 'Not a valid semver version: a.b.c',
+      range: 'a.b.c',
+      version: '1.40.0',
+    },
+  ]
+
+  tests.forEach(({ description, message, range, version }) => {
+    it(description, () => {
+      assert.throws(() => satisfiesVersionRange(version, range), message)
+    })
+  })
 })
