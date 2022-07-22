7658b0b49cce86630ffeb8bd6fcdd13f201f3870
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-07-22 16:05:37 +0100

    Update handler function signature in golang scenario handlers

--- tests/scenario/aws-golang-simple-http-endpoint/world/main.go
+++ tests/scenario/aws-golang-simple-http-endpoint/world/main.go
@@ -9,25 +9,19 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
-type Response events.APIGatewayProxyResponse
-
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	var buf bytes.Buffer
 
 	body, err := json.Marshal(map[string]interface{}{
 		"message": "Okay so your other function also executed successfully!",
 	})
 	if err != nil {
-		return Response{StatusCode: 404}, err
+		return events.APIGatewayProxyResponse{StatusCode: 404}, err
 	}
 	json.HTMLEscape(&buf, body)
 
-	resp := Response{
+	resp := events.APIGatewayProxyResponse{
 		StatusCode:      200,
 		IsBase64Encoded: false,
 		Body:            buf.String(),
