142d947fcbae9affc991580f3115a74b0f7c1b6d
TheTeaCat <joshua.r.osullivan@gmail.com>
2022-07-22 16:43:01 +0100

    Loop inside it() so beforeEach doesn't run between /hello & /world calls

--- tests/scenario/aws-golang-simple-http-endpoint.test.js
+++ tests/scenario/aws-golang-simple-http-endpoint.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-await-in-loop */
 import assert from 'node:assert'
 import { dirname, resolve } from 'node:path'
 import { env } from 'node:process'
@@ -22,9 +23,9 @@ describe('aws golang simple http endpoint', function desc() {
 
   afterEach(() => teardown())
 
-  // We GET /hello multiple times to avoid regression of https://github.com/dherault/serverless-offline/issues/1504
-  for (let i = 0; i < 3; i += 1) {
-    it('get /hello', async () => {
+  it('get /hello', async () => {
+    // We GET /hello multiple times to avoid regression of https://github.com/dherault/serverless-offline/issues/1504
+    for (let i = 0; i < 3; i += 1) {
       const url = joinUrl(env.TEST_BASE_URL, '/hello')
       const response = await fetch(url)
       const json = await response.json()
@@ -34,12 +35,12 @@ describe('aws golang simple http endpoint', function desc() {
       }
 
       assert.deepEqual(json, expected)
-    })
-  }
+    }
+  })
 
-  // We GET /world multiple times to avoid regression of https://github.com/dherault/serverless-offline/issues/1504
-  for (let i = 0; i < 3; i += 1) {
-    it('get /world', async () => {
+  it('get /world', async () => {
+    // We GET /world multiple times to avoid regression of https://github.com/dherault/serverless-offline/issues/1504
+    for (let i = 0; i < 3; i += 1) {
       const url = joinUrl(env.TEST_BASE_URL, '/world')
       const response = await fetch(url)
       const json = await response.json()
@@ -49,6 +50,6 @@ describe('aws golang simple http endpoint', function desc() {
       }
 
       assert.deepEqual(json, expected)
-    })
-  }
+    }
+  })
 })
