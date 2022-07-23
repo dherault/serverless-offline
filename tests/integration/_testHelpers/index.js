e2da574cac2211c9b5cfe1dd990e4b935dc98bcb
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-07-23 16:06:44 -0400

    test: remove unused joinUrl function

--- tests/integration/_testHelpers/index.js
+++ tests/integration/_testHelpers/index.js
@@ -1,4 +1,3 @@
 export * from './setupTeardown.js'
-export { default as joinUrl } from './joinUrl.js'
 export { default as buildInContainer } from './buildInContainer.js'
 export { default as compressArtifact } from './compressArtifact.js'
--- tests/integration/_testHelpers/joinUrl.js
+++
@@ -1,8 +0,0 @@
-export default function joinUrl(baseUrl, path) {
-  const url = new URL(baseUrl)
-  const { pathname } = url
-
-  url.pathname = pathname === '/' ? path : `${pathname}${path}`
-
-  return url
-}
