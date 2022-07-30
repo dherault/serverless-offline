2a91ad32026756d322dc768b716ea2f402e0c357
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-07-30 08:59:43 -0400

    test: move go http test to runtimes

--- tests/runtimes/go/aws-golang-simple-http-endpoint/aws-golang-simple-http-endpoint.test.js
+++ tests/runtimes/go/aws-golang-simple-http-endpoint/aws-golang-simple-http-endpoint.test.js
@@ -3,15 +3,15 @@ import assert from 'node:assert'
 import { dirname, resolve } from 'node:path'
 import { env } from 'node:process'
 import { fileURLToPath } from 'node:url'
-import { setup, teardown } from '../_testHelpers/index.js'
-import { BASE_URL } from '../config.js'
+import { setup, teardown } from '../../../_testHelpers/index.js'
+import { BASE_URL } from '../../../config.js'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 describe('aws golang simple http endpoint', function desc() {
   beforeEach(async () => {
     await setup({
-      servicePath: resolve(__dirname, 'aws-golang-simple-http-endpoint'),
+      servicePath: resolve(__dirname),
     })
   })
 
