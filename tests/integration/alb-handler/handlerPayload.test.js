7533fb725736a1a221d52b3057cf9adebf5407b2
I. Hendriks <ihendriks@protonmail.com>
2022-08-15 19:13:49 +0200

    added testcases for ALB handlers

---
+++ tests/integration/alb-handler/handlerPayload.test.js
@@ -0,0 +1,275 @@
+import assert from 'node:assert'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { setup, teardown } from '../../_testHelpers/index.js'
+import { BASE_URL } from '../../config.js'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+describe('ALB handler payload tests', function desc() {
+  beforeEach(() =>
+    setup({
+      noPrependStageInUrl: false,
+      servicePath: resolve(__dirname),
+    }),
+  )
+
+  afterEach(() => teardown())
+
+  //
+  ;[
+    {
+      description: 'when handler is context.done',
+      expected: 'foo',
+      path: '/dev/context-done-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.done which is deferred',
+      expected: 'foo',
+      path: '/dev/context-done-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.succeed',
+      expected: 'foo',
+      path: '/dev/context-succeed-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.succeed which is deferred',
+      expected: 'foo',
+      path: '/dev/context-succeed-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is a callback',
+      expected: 'foo',
+      path: '/dev/callback-handler',
+      status: 200,
+    },
+    {
+      description: 'when handler is a callback which is deferred',
+      expected: 'foo',
+      path: '/dev/callback-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler returns a promise',
+      expected: 'foo',
+      path: '/dev/promise-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler a promise which is deferred',
+      expected: 'foo',
+      path: '/dev/promise-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is an async function',
+      expected: 'foo',
+      path: '/dev/async-function-handler',
+      status: 200,
+    },
+
+    // NOTE: mix and matching of callbacks and promises is not recommended,
+    // nonetheless, we test some of the behaviour to match AWS execution precedence
+    {
+      description:
+        'when handler returns a callback but defines a callback parameter',
+      expected: 'Hello Promise!',
+      path: '/dev/promise-with-defined-callback-handler',
+      status: 200,
+    },
+
+    {
+      description:
+        'when handler throws an exception before calling callback should return 502',
+      path: '/dev/throw-exception-in-callback-handler',
+      status: 502,
+    },
+
+    {
+      description:
+        'when handler returns bad answer in promise should return 204',
+      path: '/dev/bad-answer-in-promise-handler',
+      status: 204,
+    },
+
+    {
+      description:
+        'when handler returns bad answer in callback should return 204',
+      path: '/dev/bad-answer-in-callback-handler',
+      status: 204,
+    },
+
+    // TODO: reactivate!
+    // {
+    //   description: 'when handler calls context.succeed and context.done',
+    //   expected: 'Hello Context.succeed!',
+    //   path: '/dev/context-succeed-with-context-done-handler',
+    // },
+
+    // TODO: reactivate!
+    // {
+    //   description: 'when handler calls callback and context.done',
+    //   expected: 'Hello Callback!',
+    //   path: '/dev/callback-with-context-done-handler',
+    // },
+
+    // TODO: reactivate!
+    // {
+    //   description: 'when handler calls callback and returns Promise',
+    //   expected: 'Hello Callback!',
+    //   path: '/dev/callback-with-promise-handler',
+    // },
+
+    // TODO: reactivate!
+    // {
+    //   description: 'when handler calls callback inside returned Promise',
+    //   expected: 'Hello Callback!',
+    //   path: '/dev/callback-inside-promise-handler',
+    // },
+  ].forEach(({ description, expected, path, status }) => {
+    it(description, async () => {
+      const url = new URL(path, BASE_URL)
+      url.port = url.port ? '3003' : url.port
+
+      const response = await fetch(url)
+      assert.equal(response.status, status)
+
+      if (expected) {
+        const json = await response.json()
+        assert.deepEqual(json, expected)
+      }
+    })
+  })
+})
+
+describe('ALB handler payload tests with prepend off', function desc() {
+  beforeEach(() =>
+    setup({
+      args: ['--noPrependStageInUrl'],
+      servicePath: resolve(__dirname),
+    }),
+  )
+
+  afterEach(() => teardown())
+
+  //
+  ;[
+    {
+      description: 'when handler is context.done',
+      expected: 'foo',
+      path: '/context-done-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.done which is deferred',
+      expected: 'foo',
+      path: '/context-done-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.succeed',
+      expected: 'foo',
+      path: '/context-succeed-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is context.succeed which is deferred',
+      expected: 'foo',
+      path: '/context-succeed-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is a callback',
+      expected: 'foo',
+      path: '/callback-handler',
+      status: 200,
+    },
+    {
+      description: 'when handler is a callback which is deferred',
+      expected: 'foo',
+      path: '/callback-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler returns a promise',
+      expected: 'foo',
+      path: '/promise-handler',
+      status: 200,
+    },
+
+    {
+      description: 'when handler a promise which is deferred',
+      expected: 'foo',
+      path: '/promise-handler-deferred',
+      status: 200,
+    },
+
+    {
+      description: 'when handler is an async function',
+      expected: 'foo',
+      path: '/async-function-handler',
+      status: 200,
+    },
+
+    // NOTE: mix and matching of callbacks and promises is not recommended,
+    // nonetheless, we test some of the behaviour to match AWS execution precedence
+    {
+      description:
+        'when handler returns a callback but defines a callback parameter',
+      expected: 'Hello Promise!',
+      path: '/promise-with-defined-callback-handler',
+      status: 200,
+    },
+
+    {
+      description:
+        'when handler throws an exception before calling callback should return 502',
+      path: '/throw-exception-in-callback-handler',
+      status: 502,
+    },
+
+    {
+      description:
+        'when handler returns bad answer in promise should return 204',
+      path: '/bad-answer-in-promise-handler',
+      status: 204,
+    },
+
+    {
+      description:
+        'when handler returns bad answer in callback should return 204',
+      path: '/bad-answer-in-callback-handler',
+      status: 204,
+    },
+  ].forEach(({ description, expected, path, status }) => {
+    it(description, async () => {
+      const url = new URL(path, BASE_URL)
+      url.port = url.port ? '3003' : url.port
+
+      const response = await fetch(url)
+      assert.equal(response.status, status)
+
+      if (expected) {
+        const json = await response.json()
+        assert.deepEqual(json, expected)
+      }
+    })
+  })
+})
---
+++ tests/integration/alb-handler/src/handler.js
@@ -0,0 +1,193 @@
+'use strict'
+
+const { stringify } = JSON
+
+exports.contextDoneHandler = function contextDoneHandler(event, context) {
+  context.done(null, {
+    body: stringify('foo'),
+    statusCode: 200,
+  })
+}
+
+exports.contextDoneHandlerDeferred = function contextDoneHandlerDeferred(
+  event,
+  context,
+) {
+  setTimeout(
+    () =>
+      context.done(null, {
+        body: stringify('foo'),
+        statusCode: 200,
+      }),
+    100,
+  )
+}
+
+exports.contextSucceedHandler = function contextSucceedHandler(event, context) {
+  context.succeed({
+    body: stringify('foo'),
+    statusCode: 200,
+  })
+}
+
+exports.contextSucceedHandlerDeferred = function contextSucceedHandlerDeferred(
+  event,
+  context,
+) {
+  setTimeout(
+    () =>
+      context.succeed({
+        body: stringify('foo'),
+        statusCode: 200,
+      }),
+    100,
+  )
+}
+
+exports.callbackHandler = function callbackHandler(event, context, callback) {
+  callback(null, {
+    body: stringify('foo'),
+    statusCode: 200,
+  })
+}
+
+exports.callbackHandlerDeferred = function callbackHandlerDeferred(
+  event,
+  context,
+  callback,
+) {
+  setTimeout(
+    () =>
+      callback(null, {
+        body: stringify('foo'),
+        statusCode: 200,
+      }),
+    100,
+  )
+}
+
+exports.promiseHandler = function promiseHandler() {
+  return Promise.resolve({
+    body: stringify('foo'),
+    statusCode: 200,
+  })
+}
+
+exports.promiseHandlerDeferred = function promiseDeferred() {
+  return new Promise((resolve) => {
+    setTimeout(
+      () =>
+        resolve({
+          body: stringify('foo'),
+          statusCode: 200,
+        }),
+      100,
+    )
+  })
+}
+
+exports.asyncFunctionHandler = async function asyncFunctionHandler() {
+  return {
+    body: stringify('foo'),
+    statusCode: 200,
+  }
+}
+
+// we deliberately test the case where a 'callback' is defined
+// in the handler, but a promise is being returned to protect from a
+// potential naive implementation, e.g.
+//
+// const { promisify } = 'utils'
+// const promisifiedHandler = handler.length === 3 ? promisify(handler) : handler
+//
+// if someone would return a promise, but also defines callback, without using it
+// the handler would not be returning anything
+exports.promiseWithDefinedCallbackHandler =
+  function promiseWithDefinedCallbackHandler(
+    event, // eslint-disable-line no-unused-vars
+    context, // eslint-disable-line no-unused-vars
+    callback, // eslint-disable-line no-unused-vars
+  ) {
+    return Promise.resolve({
+      body: stringify('Hello Promise!'),
+      statusCode: 200,
+    })
+  }
+
+exports.contextSucceedWithContextDoneHandler =
+  function contextSucceedWithContextDoneHandler(event, context) {
+    context.succeed({
+      body: stringify('Hello Context.succeed!'),
+      statusCode: 200,
+    })
+
+    context.done(null, {
+      body: stringify('Hello Context.done!'),
+      statusCode: 200,
+    })
+  }
+
+exports.callbackWithContextDoneHandler =
+  function callbackWithContextDoneHandler(event, context, callback) {
+    callback(null, {
+      body: stringify('Hello Callback!'),
+      statusCode: 200,
+    })
+
+    context.done(null, {
+      body: stringify('Hello Context.done!'),
+      statusCode: 200,
+    })
+  }
+
+exports.callbackWithPromiseHandler = function callbackWithPromiseHandler(
+  event,
+  context,
+  callback,
+) {
+  callback(null, {
+    body: stringify('Hello Callback!'),
+    statusCode: 200,
+  })
+
+  return Promise.resolve({
+    body: stringify('Hello Promise!'),
+    statusCode: 200,
+  })
+}
+
+exports.callbackInsidePromiseHandler = function callbackInsidePromiseHandler(
+  event,
+  context,
+  callback,
+) {
+  return new Promise((resolve) => {
+    callback(null, {
+      body: stringify('Hello Callback!'),
+      statusCode: 200,
+    })
+
+    resolve({
+      body: stringify('Hello Promise!'),
+      statusCode: 200,
+    })
+  })
+}
+
+exports.throwExceptionInPromiseHandler = async () => {
+  throw NaN
+}
+
+exports.throwExceptionInCallbackHandler = () => {
+  throw NaN
+}
+
+exports.NoAnswerInPromiseHandler = async () => {}
+
+exports.BadAnswerInPromiseHandler = async () => {
+  return {}
+}
+
+exports.BadAnswerInCallbackHandler = (event, context, callback) => {
+  callback(null, {})
+}
