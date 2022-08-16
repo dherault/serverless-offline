d213921eec04421e9390d0e73b9cc1a12e952443
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-08-16 17:59:19 -0400

    refactor: use function statements

--- src/events/http/authJWTSettingsExtractor.js
+++ src/events/http/authJWTSettingsExtractor.js
@@ -1,20 +1,24 @@
 import { log } from '@serverless/utils/log.js'
 
+function buildFailureResult(warningMessage) {
+  log.warning(warningMessage)
+
+  return {
+    unsupportedAuth: true,
+  }
+}
+
+function buildSuccessResult(authorizerName) {
+  return {
+    authorizerName,
+  }
+}
+
 export default function authJWTSettingsExtractor(
   endpoint,
   provider,
   ignoreJWTSignature,
 ) {
-  const buildFailureResult = (warningMessage) => {
-    log.warning(warningMessage)
-
-    return {
-      unsupportedAuth: true,
-    }
-  }
-
-  const buildSuccessResult = (authorizerName) => ({ authorizerName })
-
   const { authorizer } = endpoint
 
   if (!authorizer) {
