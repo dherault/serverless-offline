38aacaf9a479802f08638539ee06c5832821d5d4
dnalborczyk <dnalborczyk@gmail.com>
2022-09-14 22:28:53 -0400

    fix: api keys object check

--- src/utils/getApiKeysValues.js
+++ src/utils/getApiKeysValues.js
@@ -1,5 +1,7 @@
 export default function getApiKeysValues(apiKeys) {
   return new Set(
-    apiKeys.filter(({ value }) => value != null).map(({ value }) => value),
+    apiKeys
+      .filter((apiKey) => typeof apiKey === 'object' && apiKey.value != null)
+      .map(({ value }) => value),
   )
 }
