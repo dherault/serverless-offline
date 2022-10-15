132dd7f18eda078a79ac486c6f63763ae8321b97
dnalborczyk <dnalborczyk@gmail.com>
2022-10-15 18:51:54 -0400

    refactor: create http server

--- src/events/http/Http.js
+++ src/events/http/Http.js
@@ -4,8 +4,16 @@ import HttpServer from './HttpServer.js'
 export default class Http {
   #httpServer = null
 
+  #lambda = null
+
+  #options = null
+
+  #serverless = null
+
   constructor(serverless, options, lambda) {
-    this.#httpServer = new HttpServer(serverless, options, lambda)
+    this.#lambda = lambda
+    this.#options = options
+    this.#serverless = serverless
   }
 
   start() {
@@ -18,6 +26,12 @@ export default class Http {
   }
 
   async createServer() {
+    this.#httpServer = new HttpServer(
+      this.#serverless,
+      this.#options,
+      this.#lambda,
+    )
+
     await this.#httpServer.createServer()
   }
 
