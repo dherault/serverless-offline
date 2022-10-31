4a2df15ebfe5f0538e96b6a88810b59b6cd1feb3
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-10-31 12:02:26 -0400

    chore: test formatting nit

--- src/utils/__tests__/generateHapiPath.test.js
+++ src/utils/__tests__/generateHapiPath.test.js
@@ -13,34 +13,45 @@ describe('generateHapiPath', () => {
   it('should generate url starting with a slash', () => {
     const options = {}
     const result = generateHapiPath('users', options, serverless)
+
     assert.equal(result[0], '/')
   })
 
   it('should generate url with the stage prepended', () => {
     const options = {}
     const result = generateHapiPath('users', options, serverless)
+
     assert.equal(result, '/dev/users')
   })
 
   describe('when a prefix option is set', () => {
     it('the url should add the prefix', () => {
-      const options = { prefix: 'some-prefix' }
+      const options = {
+        prefix: 'some-prefix',
+      }
       const result = generateHapiPath('users', options, serverless)
+
       assert.equal(result, '/some-prefix/dev/users')
     })
   })
 
   describe('when the noPrependStageInUrl option is set', () => {
     it('the url should omit the stage', () => {
-      const options = { noPrependStageInUrl: true }
+      const options = {
+        noPrependStageInUrl: true,
+      }
       const result = generateHapiPath('users', options, serverless)
+
       assert.equal(result, '/users')
     })
   })
 
   it('the stage from options should override stage from serverless config', () => {
-    const options = { stage: 'prod' }
+    const options = {
+      stage: 'prod',
+    }
     const result = generateHapiPath('users', options, serverless)
+
     assert.equal(result, '/prod/users')
   })
 })
