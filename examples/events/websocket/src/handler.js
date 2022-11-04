70bc8ecf54b1c5daecac325ebe2a97db1ebe11b5
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-11-04 10:24:00 -0400

    chore: fix websocket example, add one-way message

--- examples/events/websocket/src/handler.js
+++ examples/events/websocket/src/handler.js
@@ -1,8 +1,13 @@
-const { stringify } = JSON
+import {
+  ApiGatewayManagementApi,
+  PostToConnectionCommand,
+} from '@aws-sdk/client-apigatewaymanagementapi'
 
-export async function connect(event, context) {
-  return {
-    body: stringify(
+const { parse, stringify } = JSON
+
+export async function initiateOneWay(event, context) {
+  console.log(
+    stringify(
       {
         context,
         event,
@@ -10,13 +15,71 @@ export async function connect(event, context) {
       null,
       2,
     ),
+  )
+
+  const body = parse(event.body)
+
+  const url = 'http://localhost:3001'
+
+  const apigatewaymanagementapi = new ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: url,
+  })
+
+  console.log('event.headers.connectionId', event.headers.connectionId)
+
+  const postToConnectionCommand = new PostToConnectionCommand({
+    ConnectionId: event.headers['connection-id'], // connectionId of the receiving ws-client
+    Data: new TextEncoder().encode(stringify(body)),
+  })
+
+  let data
+
+  try {
+    data = await apigatewaymanagementapi.send(postToConnectionCommand)
+  } catch (err) {
+    console.log('err is', err)
+    throw err
+  }
+
+  console.log(data)
+
+  return {
+    body: stringify({
+      status: 'Message send.',
+    }),
+    statusCode: 200,
+  }
+}
+
+export async function connect(event, context) {
+  console.log(
+    stringify(
+      {
+        context,
+        event,
+      },
+      null,
+      2,
+    ),
+  )
+
+  return {
+    // body: stringify(
+    //   {
+    //     context,
+    //     event,
+    //   },
+    //   null,
+    //   2,
+    // ),
     statusCode: 200,
   }
 }
 
 export async function disconnect(event, context) {
-  return {
-    body: stringify(
+  console.log(
+    stringify(
       {
         context,
         event,
@@ -24,17 +87,38 @@ export async function disconnect(event, context) {
       null,
       2,
     ),
+  )
+
+  return {
+    // body: stringify(
+    //   {
+    //     context,
+    //     event,
+    //   },
+    //   null,
+    //   2,
+    // ),
     statusCode: 200,
   }
 }
 
 // eslint-disable-next-line no-underscore-dangle
-export async function _default(event, context) {
-  return {
-    body: stringify(
+export async function defaultRoute(event, context) {
+  console.log(
+    stringify(
       {
         context,
         event,
+      },
+      null,
+      2,
+    ),
+  )
+
+  return {
+    body: stringify(
+      {
+        Hello: 'World',
       },
       null,
       2,
