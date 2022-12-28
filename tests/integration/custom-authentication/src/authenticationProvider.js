606fab5ef340fe86166b71c63e6e87cf9024452f
Daniel Nalborczyk <dnalborczyk@gmail.com>
2022-12-27 19:45:56 -0500

    chore: remove eslint-disable-next-line

--- tests/integration/custom-authentication/src/authenticationProvider.js
+++ tests/integration/custom-authentication/src/authenticationProvider.js
@@ -1,5 +1,6 @@
-// eslint-disable-next-line no-unused-vars
-module.exports = (endpoint, functionKey, method, path) => {
+'use strict'
+
+module.exports = () => {
   return {
     getAuthenticateFunction() {
       return {
