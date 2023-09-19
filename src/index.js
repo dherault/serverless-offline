8ea15b5a6d924c0d612d520e998eb50edc53de75
Daniel Nalborczyk <dnalborczyk@gmail.com>
2023-09-18 22:32:39 -0400

    refactor: remove Object.hasOwn polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// TODO remove with node.js v16.9+ support
-import 'object.hasown/auto'
-
 // install global fetch
 // TODO remove `node-fetch` module and use global built-in with node.js v18+ support
 if (globalThis.fetch === undefined) {
