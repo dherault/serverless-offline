522f3efa8e24f05ff36b58e18f8b80b98a9f6acf
Christian Nuss <christian.nuss@gmail.com>
2024-04-15 21:55:13 -0700

    add a test

--- tests/integration/httpApi-headers/httpApi-headers.test.js
+++ tests/integration/httpApi-headers/httpApi-headers.test.js
@@ -1,4 +1,5 @@
 import assert from "node:assert"
+import { Buffer } from "node:buffer"
 import { join } from "desm"
 import { setup, teardown } from "../../_testHelpers/index.js"
 import { BASE_URL } from "../../config.js"
@@ -32,6 +33,32 @@ describe("HttpApi Headers Tests", function desc() {
 
       assert.equal(body.headersReceived.origin, "http://www.example.com")
       assert.equal(body.headersReceived["x-webhook-signature"], "ABCDEF")
+      assert.equal(body.isBase64Encoded, false)
     })
+  })
+
+  it(`multipart/form-data headers are base64 encoded`, async () => {
+    const url = new URL("/echo-headers", BASE_URL)
+    const options = {
+      body: `------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name="file"; filename="file.txt"\r\nContent-Type: text/plain\r\n\r\ncontent\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--\r\n`,
+      headers: {
+        "Content-Type":
+          "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        Origin: "http://www.example.com",
+        "X-Webhook-Signature": "ABCDEF",
+      },
+      method: "POST",
+    }
+
+    const response = await fetch(url, options)
+
+    assert.equal(response.status, 200)
+
+    const body = await response.json()
+
+    assert.equal(body.headersReceived.origin, "http://www.example.com")
+    assert.equal(body.headersReceived["x-webhook-signature"], "ABCDEF")
+    assert.equal(body.isBase64Encoded, true)
+    assert.equal(body.body, Buffer.from(options.body).toString("base64"))
   })
 })
--- tests/integration/httpApi-headers/src/handler.js
+++ tests/integration/httpApi-headers/src/handler.js
@@ -3,7 +3,9 @@ const { stringify } = JSON
 export async function echoHeaders(event) {
   return {
     body: stringify({
+      body: event.body,
       headersReceived: event.headers,
+      isBase64Encoded: event.isBase64Encoded,
     }),
     statusCode: 200,
   }
