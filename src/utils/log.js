8904098239dc6b5c681c74a44681f07a7f0b38aa
Tomasz Czubocha <tomasz.czubocha@gmail.com>
2024-05-09 22:45:20 +0200

    refactor: use provided log utils

--- src/utils/log.js
+++ src/utils/log.js
@@ -5,16 +5,16 @@ export function setLogUtils({ log, progress }) {
 }
 
 export const log = {
-  debug: (...args) => utils?.log.debug(...args),
-  error: (...args) => utils?.log.error(...args),
-  info: (...args) => utils?.log.info(...args),
-  notice: (...args) => utils?.log.notice(...args),
-  verbose: (...args) => utils?.log.verbose(...args),
-  warning: (...args) => utils?.log.warning(...args),
+  debug: (...args) => utils?.log?.debug(...args),
+  error: (...args) => utils?.log?.error(...args),
+  info: (...args) => utils?.log?.info(...args),
+  notice: (...args) => utils?.log?.notice(...args),
+  verbose: (...args) => utils?.log?.verbose(...args),
+  warning: (...args) => utils?.log?.warning(...args),
 }
 
 export const progress = {
-  get: (namespace) => utils?.progress.get(namespace),
-  notice: (message) => utils?.progress.notice(message),
-  remove: () => utils?.progress.remove(),
+  get: (namespace) => utils?.progress?.get(namespace),
+  notice: (message) => utils?.progress?.notice(message),
+  remove: () => utils?.progress?.remove(),
 }
